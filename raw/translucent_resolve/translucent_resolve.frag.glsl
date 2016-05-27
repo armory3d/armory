63ff6726bbd57d1bbbaf8d33855989db44fab068
Lubos Lenco <lubos.lenco@gmail.com>
2016-05-27 09:46:35 +0200

    Fix translucent resolve.

--- raw/translucent_resolve/translucent_resolve.frag.glsl
+++ raw/translucent_resolve/translucent_resolve.frag.glsl
@@ -12,14 +12,15 @@ uniform sampler2D gbuffer1; // revealageTexture
 in vec2 texCoord;
 
 void main() {
-	float revealage = texture(gbuffer1, texCoord).r;
+	vec4 accum = texture(gbuffer0, texCoord);
+	float revealage = accum.a;
 	
 	if (revealage == 1.0) {
         // Save the blending and color texture fetch cost
         discard; 
     }
 	
-	vec4 accum = texture(gbuffer0, texCoord);
+	accum.a = texture(gbuffer1, texCoord).r;
 	
 	// gl_FragColor = vec4(accum.rgb / clamp(accum.a, 1e-4, 5e4), revealage);
 
