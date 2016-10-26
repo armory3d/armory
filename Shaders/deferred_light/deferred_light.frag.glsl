8e21184c378853b47752ca49efb95729c043a848
luboslenco <lubos.lenco@gmail.com>
2016-10-26 13:50:17 +0200

    Depth fetch workaround.

--- Shaders/deferred_light/deferred_light.frag.glsl
+++ Shaders/deferred_light/deferred_light.frag.glsl
@@ -99,10 +99,12 @@ void main() {
 	vec2 texCoord = screenPosition * 0.5 + 0.5;
 	// texCoord += vec2(0.5 / screenSize); // Half pixel offset
 
-	// 0 - 1 => -1 - 1
-	float depth = texture(gbufferD, texCoord).r * 2.0 - 1.0;
 	vec4 g0 = texture(gbuffer0, texCoord); // Normal.xy, metallic/roughness, mask
 	vec4 g1 = texture(gbuffer1, texCoord); // Basecolor.rgb, occlusion
+	// 0 - 1 => -1 - 1
+	// float depth = texture(gbufferD, texCoord).r * 2.0 - 1.0;
+	// TODO: Can not read and write to depth buffer at once, fetch depth from g0
+	float depth = (1.0 - g0.a) * 2.0 - 1.0;
 
 	vec3 n;
 	n.z = 1.0 - abs(g0.x) - abs(g0.y);
