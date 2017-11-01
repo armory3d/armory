62c9a9ce1bedd0f74c572f7c0596e9cf60943bcc
Lubos Lenco <lubos.lenco@gmail.com>
2017-11-01 15:23:28 +0100

    Fix unparenting

--- Sources/armory/logicnode/SetParentNode.hx
+++ Sources/armory/logicnode/SetParentNode.hx
@@ -4,21 +4,23 @@ import armory.object.Object;
 
 class SetParentNode extends LogicNode {
 
-	static inline var parentInverse = true;
-
 	public function new(tree:LogicTree) {
 		super(tree);
 	}
 
 	override function run() {
 		var object:Object = inputs[1].get();
-		var parent:Object = inputs[2].get();
+
+		var parent:Object;
+		var parentNode = cast(inputs[2].node, ObjectNode);
+		var isUnparent = parentNode.objectName == "";
+		if (isUnparent) parent = iron.Scene.active.root;
+		else parent = inputs[2].get();
 		
-		if (object == null) return;
-		if (parent == null) parent = tree.object;
+		if (object == null || parent == null) return;
 
 		object.parent.children.remove(object);
-		parent.addChild(object, parentInverse);
+		parent.addChild(object, !isUnparent); // applyInverse
 
 		super.run();
 	}
