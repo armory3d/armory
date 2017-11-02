7fed0a24c946e17001f24ecfa9b58e459b54101b
Lubos Lenco <lubos.lenco@gmail.com>
2017-11-02 23:32:36 +0100

    Array length node

---
+++ blender/arm/logicnode/array_length.py
@@ -0,0 +1,16 @@
+import bpy
+from bpy.props import *
+from bpy.types import Node, NodeSocket
+from arm.logicnode.arm_nodes import *
+
+class ArrayLengthNode(Node, ArmLogicTreeNode):
+    '''Array length node'''
+    bl_idname = 'LNArrayLengthNode'
+    bl_label = 'Array Length'
+    bl_icon = 'GAME'
+
+    def init(self, context):
+        self.inputs.new('NodeSocketShader', 'Array')
+        self.outputs.new('NodeSocketInt', 'Length')
+
+add_node(ArrayGetNode, category='Array')
