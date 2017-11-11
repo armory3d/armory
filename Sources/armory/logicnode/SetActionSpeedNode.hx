7d9426c58bedaa5e5dfa6d5b6fb5cd60344f2eb3
Lubos Lenco <lubos.lenco@gmail.com>
2017-11-11 16:46:43 +0100

    Set Action Speed node

---
+++ Sources/armory/logicnode/SetActionSpeedNode.hx
@@ -0,0 +1,24 @@
+package armory.logicnode;
+
+import armory.object.Object;
+
+class SetActionSpeedNode extends LogicNode {
+
+	public function new(tree:LogicTree) {
+		super(tree);
+	}
+
+	override function run() {
+		var object:Object = inputs[1].get();
+		var speed:Float = inputs[2].get();
+		
+		if (object == null) return;
+
+		// Try first child if we are running from armature
+		if (object.animation == null) object = object.children[0];
+
+		object.animation.speed = speed;
+
+		super.run();
+	}
+}
