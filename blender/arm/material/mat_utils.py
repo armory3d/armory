e8a4e021b0c91d7e6e0aa08429f8f39385da23f7
Lubos Lenco <lubos.lenco@gmail.com>
2017-11-26 15:03:11 +0100

    Use mesh context for disabled translucency

--- blender/arm/material/mat_utils.py
+++ blender/arm/material/mat_utils.py
@@ -30,7 +30,7 @@ def get_rpasses(material):
         ar.append('decal')
     elif material.arm_overlay:
         ar.append('overlay')
-    elif is_transluc(material) and not material.arm_discard and not vgirefract:
+    elif is_transluc(material) and not material.arm_discard and not vgirefract and rpdat.rp_translucency_state != 'Off':
         ar.append('translucent')
     else:
         ar.append('mesh')
