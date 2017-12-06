1e19a9d0dc24038293070d2f89cfba1728a460ad
Martin Wallin <guzzard@gmail.com>
2017-12-06 22:20:42 +0100

    Get/Set CameraObject FOV logic nodes

---
+++ blender/arm/logicnode/action_set_camera_fov.py
@@ -0,0 +1,18 @@
+import bpy
+from bpy.props import *
+from bpy.types import Node, NodeSocket
+from arm.logicnode.arm_nodes import *
+
+class SetCameraFovNode(Node, ArmLogicTreeNode):
+    '''Set camera FOV node'''
+    bl_idname = 'LNSetCameraFovNode'
+    bl_label = 'Set Camera FOV'
+    bl_icon = 'GAME'
+
+    def init(self, context):
+        self.inputs.new('ArmNodeSocketAction', 'In')
+        self.inputs.new('ArmNodeSocketObject', 'Object')
+        self.inputs.new('NodeSocketFloat', 'FOV')
+        self.outputs.new('ArmNodeSocketAction', 'Out')
+
+add_node(SetCameraFovNode, category='Action')
