93dbcde41087c41bdcead2fa13ec4bf259ae6669
Martin Wallin <guzzard@gmail.com>
2017-12-20 17:51:25 +0100

    Bundled script: follow camera

---
+++ Sources/armory/trait/FollowCamera.hx
@@ -0,0 +1,52 @@
+package armory.trait;
+
+import iron.Scene;
+import iron.object.Object;
+import iron.math.Vec4;
+
+/*
+	This trait is to be used with a camera mounted on a camera boom with offset.
+	1. Place the camera as a child to another object, for example an 'Empty'.
+	2. Place this trait on the 'Empty' object.
+	3. Set the name of the target object to be followed by the camera.
+*/
+class FollowCamera extends iron.Trait {
+
+	@prop
+	var target:String;
+
+	@prop
+	var lerpSpeed:Float = 0.1;
+
+	var targetObj:Object;
+	var disabled = false;
+
+	public function new() {
+		super();
+
+		notifyOnInit(function() {
+			targetObj = Scene.active.getChild(target);
+			if(targetObj == null){
+				disabled = true;
+				trace("FollowCamera error, unable to set target object");
+			}
+
+			if(Type.getClass(object) == iron.object.CameraObject){
+			 	disabled = true;
+				trace("FollowCamera error, this trait should not be placed directly on a camera objet. It should be placed on another object such as an Empty. The camera should be placed as a child to the Empty object with offset, creating a camera boom.");
+			}
+		});
+
+		notifyOnUpdate(function() {
+			if(!disabled){
+				if(targetObj != null) {
+					var vec = Vec4.lerp(targetObj.transform.world.getLoc(), object.transform.world.getLoc(), lerpSpeed);
+					object.transform.loc = vec;
+					object.transform.buildMatrix();
+				} else {
+					targetObj = Scene.active.getChild(target);
+				}
+			}
+		});
+	}
+}
