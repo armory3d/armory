1c5665384936f9ee4ba7afcbf89c27d973d1ed3b
Lubos Lenco <lubos.lenco@gmail.com>
2018-01-28 14:03:56 +0100

    Improve volumetrics filtering

--- blender/arm/make_renderpath.py
+++ blender/arm/make_renderpath.py
@@ -275,7 +275,8 @@ def build():
         assets.add_shader_pass('volumetric_light')
         assets.add_shader_pass('blur_bilat_pass')
         assets.add_shader_pass('blur_bilat_blend_pass')
-        assets.add_embedded_data('noise8.png')
+        assets.add(assets_path + 'blue_noise64.png')
+        assets.add_embedded_data('blue_noise64.png')
 
     if rpdat.rp_decals:
         assets.add_khafile_def('rp_decals')
