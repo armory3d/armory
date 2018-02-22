2a3f0a799854fdb588f5f5ec387377460fcb0d4d
Lubos Lenco <lubos.lenco@gmail.com>
2018-02-22 13:25:06 +0100

    Add Set Parent Bone node

---
+++ Sources/armory/logicnode/SetParentBoneNode.hx
@@ -0,0 +1,28 @@
+package armory.logicnode;
+
+import iron.object.Object;
+
+class SetParentBoneNode extends LogicNode {
+
+	public function new(tree:LogicTree) {
+		super(tree);
+	}
+
+	override function run() {
+		var object:Object = inputs[1].get();
+		var parent:Object = inputs[2].get();
+		var bone:String = inputs[3].get();
+		
+		if (object == null || parent == null) return;
+
+		if (object.parent != parent) {
+			object.parent.removeChild(object, false); // keepTransform
+			parent.addChild(object, true); // applyInverse
+		}
+
+		var banim = object.getParentArmature(object.parent.name);
+		banim.addBoneChild(bone, object);
+
+		super.run();
+	}
+}
