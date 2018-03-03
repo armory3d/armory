f456cc9c0229903008c4c87faad3c475a3efb4a2
luboslenco <lubos.lenco@gmail.com>
2018-03-03 13:09:08 +0100

    Fix event removal

--- Sources/armory/logicnode/OnEventNode.hx
+++ Sources/armory/logicnode/OnEventNode.hx
@@ -1,12 +1,16 @@
 package armory.logicnode;
 
+import armory.system.Event;
+
 class OnEventNode extends LogicNode {
 
 	var _property0:String;
 	public var property0(get, set):String;
+	var listener:TEvent = null;
 
 	public function new(tree:LogicTree) {
 		super(tree);
+		tree.notifyOnRemove(onRemove);
 	}
 
 	function get_property0():String {
@@ -14,11 +18,15 @@ class OnEventNode extends LogicNode {
 	}
 
 	function set_property0(s:String):String {
-		armory.system.Event.add(s, onEvent, tree.object.uid);
+		listener = Event.add(s, onEvent, tree.object.uid);
 		return _property0 = s;
 	}
 
 	function onEvent() {
 		run();
+	}
+
+	function onRemove() {
+		if (listener != null) Event.removeListener(listener);
 	}
 }
