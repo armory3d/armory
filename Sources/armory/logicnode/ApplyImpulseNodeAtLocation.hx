00056b8e0ccc55eec529e743ab5602760ba28b9a
Andrei Nadin <anadin@me.com>
2018-04-13 13:29:05 +1000

    Apply Impulse at location Node

---
+++ Sources/armory/logicnode/ApplyImpulseNodeAtLocation.hx
@@ -0,0 +1,27 @@
+package armory.logicnode;
+
+import iron.object.Object;
+import iron.math.Vec4;
+import armory.trait.physics.RigidBody;
+
+class ApplyImpulseNodeAtLocation extends LogicNode {
+
+	public function new(tree:LogicTree) {
+		super(tree);
+	}
+
+	override function run() {
+		var object:Object = inputs[1].get();
+		var impulse:Vec4 = inputs[2].get();
+		var location:Vec4 = inputs[3].get();
+		
+		if (object == null) return;
+
+#if arm_physics
+		var rb:RigidBody = object.getTrait(RigidBody);
+		rb.applyImpulse(impulse,location);
+#end
+
+		super.run();
+	}
+}
