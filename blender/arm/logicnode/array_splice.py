f8f0e127a13ef1b4f40c110375939d0393d5a87b
unknown <lubos.lenco@gmail.com>
2018-06-24 15:47:39 +0200

    Add Array Slice and Array Splice nodes

---
+++ blender/arm/logicnode/array_splice.py
@@ -0,0 +1,19 @@
+import bpy
+from bpy.props import *
+from bpy.types import Node, NodeSocket
+from arm.logicnode.arm_nodes import *
+
+class ArraySpliceNode(Node, ArmLogicTreeNode):
+    '''Array splice node'''
+    bl_idname = 'LNArraySpliceNode'
+    bl_label = 'Array Splice'
+    bl_icon = 'GAME'
+
+    def init(self, context):
+        self.inputs.new('ArmNodeSocketAction', 'In')
+        self.inputs.new('ArmNodeSocketArray', 'Array')
+        self.inputs.new('NodeSocketInt', 'Index')
+        self.inputs.new('NodeSocketInt', 'Length')
+        self.outputs.new('ArmNodeSocketAction', 'Out')
+
+add_node(ArraySpliceNode, category='Array')
