e1383f4439c5904ea6d4fb4e6164f68842253e48
luboslenco <lubos.lenco@gmail.com>
2018-08-08 15:06:10 +0200

    Handle blending pass in forward path

--- Sources/armory/renderpath/RenderPathForward.hx
+++ Sources/armory/renderpath/RenderPathForward.hx
@@ -15,6 +15,13 @@ class RenderPathForward {
 
 	public static function drawMeshes() {
 		path.drawMeshes("mesh");
+
+		#if rp_blending
+		{
+			path.drawMeshes("blend");
+		}
+		#end
+		
 		#if (rp_background == "World")
 		{
 			path.drawSkydome("shader_datas/world_pass/world_pass");
