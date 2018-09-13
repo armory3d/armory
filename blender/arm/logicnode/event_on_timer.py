9249fe79db27ac7a63704a921e7f2f9d7b8b07f9
thinkinmonkey <thinkinmonkey@gmail.com>
2018-09-13 10:07:12 +0100

    new onTimer node as requested on #612

--- blender/arm/logicnode/event_on_timer.py
+++ blender/arm/logicnode/event_on_timer.py
@@ -4,14 +4,23 @@ from bpy.types import Node, NodeSocket
 from arm.logicnode.arm_nodes import *
 
 class OnTimerNode(Node, ArmLogicTreeNode):
-    '''On timer node'''
-    bl_idname = 'LNOnTimerNode'
-    bl_label = 'On Timer'
-    bl_icon = 'CURVE_PATH'
-    
-    def init(self, context):
-        self.inputs.new('NodeSocketFloat', 'Duration')
-        self.inputs.new('NodeSocketBool', 'Repeat')
-        self.outputs.new('ArmNodeSocketAction', 'Out')
+	'''On timer node'''
+	bl_idname = 'LNOnTimerNode'
+	bl_label = 'On Timer'
+	bl_icon = 'CURVE_PATH'
+	
+	def init(self, context):
+		self.inputs.new('ArmNodeSocketAction', 'In')
+		self.inputs.new('NodeSocketFloat', 'Duration')
+		self.inputs.new('NodeSocketInt', 'Repeat')
+		self.inputs.new('NodeSocketBool', 'Pause')
 
+		self.outputs.new('ArmNodeSocketAction', 'Out')
+		self.outputs.new('NodeSocketBool', 'Running')
+		self.outputs.new('NodeSocketInt', 'secondsPassed')
+		self.outputs.new('NodeSocketInt', 'secondsLeft')
+		self.outputs.new('NodeSocketFloat', 'Normalized')
+	
+		self.inputs['Repeat'].default_value = 1
+		
 add_node(OnTimerNode, category='Event')
