f03dc185903408d13991a795dfff352bedc09ab7
unknown <lubos.lenco@gmail.com>
2018-10-21 20:43:28 +0200

    Format

--- Sources/armory/logicnode/TimerNode.hx
+++ Sources/armory/logicnode/TimerNode.hx
@@ -5,59 +5,44 @@ class TimerNode extends LogicNode {
 	var duration = 0.0;
 	var initialDuration = 0.0;
 	var repeat = 0;
-	var pause = false;
-	public var running:Bool = false;
-	public var secondsPassed:Int = 0;
-	public var secondsLeft:Int = 0;
-	public var normalized:Float = 0.0;
+	var running = false;
+	var repetitions = 0;
 
 	public function new(tree:LogicTree) {
 		super(tree);
 	}
 
 	function update() {
-		if (pause == false) {
-			super.run();
-			if (repeat > 0) {
-				this.running = true;
-				if (this.running == true) {
-					duration -= iron.system.Time.delta;
-					this.secondsPassed = Std.int(initialDuration - duration);
-					this.secondsLeft = Std.int(duration+1);
-					this.normalized = Math.min(Math.max(0.0, 1 - (duration / initialDuration)), 1.0);
-					
-					if (duration < 0.0) {
-						repeat -= 1;
-						duration = initialDuration;
-						this.running = false;
-					}
-				}
+		duration -= iron.system.Time.delta;
+		if (duration <= 0.0) {
+			repeat--;
+			if (repeat != 0) {
+				duration = initialDuration;
+				repetitions++;
 			}
 			else {
-				this.running = false;
+				running = false;
 				tree.removeUpdate(update);
 			}
+			super.runOutputs(0);
 		}
 	}
 	
 	override function run() {
-		duration = inputs[1].get();
-		initialDuration = inputs[1].get();
-		repeat = inputs[2].get();
-		pause = inputs[3].get();
+		initialDuration = inputs[3].get();
+		repeat = inputs[4].get();
 		
-		this.running = true;
-
-		if (pause == false) {
-			tree.notifyOnUpdate(update);
-		}
+		running = true;
+		duration = initialDuration;
+		repetitions = 0;
+		tree.notifyOnUpdate(update);
 	}
 
 	override function get(from:Int):Dynamic {
-		if (from == 1) return this.running;
-		else if (from == 2) return this.secondsPassed;
-		else if (from == 3) return this.secondsLeft;
-		else return this.normalized;
+		if (from == 1) return running;
+		else if (from == 2) return initialDuration - duration;
+		else if (from == 3) return duration;
+		else if (from == 4) return (1.0 - (duration / initialDuration));
+		else return repetitions;
 	}
-
 }
