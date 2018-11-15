cc960e76574e50c089896612f60c1a7bd893aef9
Zicklag <zicklag@katharostech.com>
2018-11-14 20:07:53 -0600

    Add Project Package Prop to Mod Panel

--- blender/arm/props_ui.py
+++ blender/arm/props_ui.py
@@ -358,9 +358,9 @@ class ArmProjectModdingPanel(bpy.types.Panel):
             row = box.row()
             col = row.column()
             col.prop(wrd, 'arm_modding_game_blend')
+            col.prop(wrd, 'arm_project_package')
             col.prop(wrd, 'arm_modding_folder')
-            row = box.row()
-            row.prop(wrd, 'arm_modding_extra_packages')
+            col.prop(wrd, 'arm_modding_extra_packages')
 
 class ArmProjectFlagsPanel(bpy.types.Panel):
     bl_label = "Flags"
