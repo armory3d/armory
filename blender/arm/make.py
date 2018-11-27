d19473a3948183d21dd27210e9894898c633e26b
luboslenco <lubos.lenco@gmail.com>
2018-11-27 20:22:59 +0100

    Use essl300 when legacy shaders are disabled

--- blender/arm/make.py
+++ blender/arm/make.py
@@ -231,7 +231,8 @@ def compile(assets_only=False):
         cmd.append('--shaderversion')
         cmd.append('110')
     elif 'android' in state.target or 'ios' in state.target or 'html5' in state.target:
-        pass # Use defaults
+        cmd.append('--shaderversion')
+        cmd.append('300')
     else:
         cmd.append('--shaderversion')
         cmd.append('330')
