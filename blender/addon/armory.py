e3c4a9855d48034f04c3c7dea3b588ced6931a43
luboslenco <lubos.lenco@gmail.com>
2019-05-05 17:24:29 +0200

    Show date-based version

--- blender/addon/armory.py
+++ blender/addon/armory.py
@@ -6,7 +6,7 @@ bl_info = {
     "location": "Properties -> Render -> Armory Player",
     "description": "3D Game Engine for Blender",
     "author": "Armory3D.org",
-    "version": (0, 7, 0),
+    "version": (2019, 5, 0),
     "blender": (2, 80, 0),
     "wiki_url": "https://armory3d.org/manual",
     "tracker_url": "https://github.com/armory3d/armory/issues"
