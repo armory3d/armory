b06c665676856f6c8626b007fb335cb59abc68ef
N8n5h <42382648+N8n5h@users.noreply.github.com>
2019-10-14 19:13:18 -0300

    Revert offset to the y value when adding a path
    This is to revert it to the original without the offset because it has inconsistent results with meshes with no height, something that I need to investigate why. 
    
    So for now to keep it consistent it stays without the offset.

--- Sources/armory/trait/NavMesh.hx
+++ Sources/armory/trait/NavMesh.hx
@@ -68,7 +68,7 @@ class NavMesh extends Trait {
 		if (!ready) return;
 		recast.findPath(from.x, from.z, from.y, to.x, to.z, to.y, 200, function(path:Array<RecastWaypoint>) {
 			var ar:Array<Vec4> = [];
-			for (p in path) ar.push(new Vec4(p.x, p.z, (p.y + 1.1)));
+			for (p in path) ar.push(new Vec4(p.x, p.z, p.y));
 			done(ar);
 		});
 	}
