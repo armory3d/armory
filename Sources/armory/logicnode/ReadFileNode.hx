5d521e8101146f7210b589cb9734ad6a10d5e3c0
Sandy <48373918+Sandy10000@users.noreply.github.com>
2019-10-17 22:04:14 +0900

    fix https://github.com/armory3d/armory/issues/1438

--- Sources/armory/logicnode/ReadFileNode.hx
+++ Sources/armory/logicnode/ReadFileNode.hx
@@ -9,12 +9,17 @@ class ReadFileNode extends LogicNode {
 	}
 
 	override function run(from:Int) {
+		// Whether to use the cache
+		var useCache:Bool = inputs[1].get();
+
 		// Relative or absolute path to file
-		var file:String = inputs[1].get();
+		var file:String = inputs[2].get();
 
 		// Load the file asynchronously
+		if(!useCache && iron.data.Data.cachedBlobs.get(file) != null) iron.data.Data.cachedBlobs.remove(file);
 		iron.data.Data.getBlob(file, function(b:kha.Blob) {
 			data = b.toString();
+			if(!useCache) iron.data.Data.cachedBlobs.remove(file);
 			runOutput(0);
 		});
 	}
