d7e6ec1a7967813df90f997ec1da45b4c217bedb
Moritz Brückner <mail@moritz-brueckner.de>
2020-04-10 00:48:49 +0200

    Fix write_matrix()

--- blender/arm/exporter.py
+++ blender/arm/exporter.py
@@ -75,7 +75,7 @@ class ArmoryExporter:
     """Export to Armory format"""
 
     @staticmethod
-    def write_matrix(self, matrix):
+    def write_matrix(matrix):
         return [matrix[0][0], matrix[0][1], matrix[0][2], matrix[0][3],
                 matrix[1][0], matrix[1][1], matrix[1][2], matrix[1][3],
                 matrix[2][0], matrix[2][1], matrix[2][2], matrix[2][3],
