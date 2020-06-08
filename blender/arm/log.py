107c61ad3e7fa4666815e864dadb3d5dc07c3989
tong <tong@disktree.net>
2020-06-08 12:26:17 +0200

    Colored terminal output

--- blender/arm/log.py
+++ blender/arm/log.py
@@ -1,3 +1,9 @@
+DEBUG = 36
+INFO = 37
+WARN = 35
+ERROR = 31
+
+no_colors = False
 info_text = ''
 num_warnings = 0
 
@@ -10,12 +16,27 @@ def clear(clear_warnings=False):
 def format_text(text):
     return (text[:80] + '..') if len(text) > 80 else text # Limit str size
 
-def print_info(text):
-    global info_text
+def log(text,color=None):
+    if not no_colors and color is not None:
+        csi = '\033['
+        text = csi + str(color) + 'm' + text + csi + '0m';
     print(text)
+
+def debug(text):
+    log(text,DEBUG)
+
+def info(text):
+    global info_text
+    log(text,INFO)
     info_text = format_text(text)
+
+def print_warn(text):
+    log('Warning: ' + text,WARN)
 
 def warn(text):
     global num_warnings
     num_warnings += 1
-    print('Armory Warning: ' + text)
+    print_warn(text)
+
+def error(text):
+    log('ERROR: ' + text,ERROR)
