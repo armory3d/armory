193c712bf45cd31cd19bf48392dc5f47bdd93f73
knowledgenude <63247726+knowledgenude@users.noreply.github.com>
2020-09-06 15:10:51 -0300

    Improve Set Activation Node

--- blender/arm/logicnode/physics_set_activation_state.py
+++ blender/arm/logicnode/physics_set_activation_state.py
@@ -3,16 +3,25 @@ from bpy.props import *
 from bpy.types import Node, NodeSocket
 from arm.logicnode.arm_nodes import *
 
-class SetActivationStateNode (Node, ArmLogicTreeNode):
+class SetActivationStateNode(Node, ArmLogicTreeNode):
     '''Set Activation State Node'''
     bl_idname = 'LNSetActivationStateNode'
     bl_label = 'Set Activation State'
     bl_icon = 'NONE'
+    property0: EnumProperty(
+        items = [('Inactive', 'Inactive', 'Inactive'),
+                 ('Active', 'Active', 'Active'),
+                 ('Always Active', 'Always Active', 'Always Active'),
+                 ('Always Inactive', 'Always Inactive', 'Always Inactive'),
+                 ],
+        name='', default='Inactive')
 
     def init(self, context):
         self.inputs.new('ArmNodeSocketAction', 'In')
         self.inputs.new('ArmNodeSocketObject', 'Object')
-        self.inputs.new('NodeSocketInt', 'State')
         self.outputs.new('ArmNodeSocketAction', 'Out')
+
+    def draw_buttons(self, context, layout):
+        layout.prop(self, 'property0')
 
 add_node(SetActivationStateNode, category='Physics')
