cfd6b01f0a4242079c7063bc80e4d663c540f83e
Moritz Brückner <mail@moritz-brueckner.de>
2020-09-09 00:32:38 +0200

    Remove alternative category icons

--- blender/arm/logicnode/__init__.py
+++ blender/arm/logicnode/__init__.py
@@ -4,10 +4,10 @@ import pkgutil
 from arm.logicnode import arm_nodes
 
 # Register node menu categories
-arm_nodes.add_category('Logic', icon='OUTLINER', section="basic") #MOD_MASK | SELECT_DIFFERENCE
+arm_nodes.add_category('Logic', icon='OUTLINER', section="basic")
 arm_nodes.add_category('Event', icon='INFO', section="basic")
-arm_nodes.add_category('Input', icon='GREASEPENCIL', section="basic") #EVENT_RETURN
-arm_nodes.add_category('Native', icon='MEMORY', section="basic") #SYSTEM
+arm_nodes.add_category('Input', icon='GREASEPENCIL', section="basic")
+arm_nodes.add_category('Native', icon='MEMORY', section="basic")
 
 arm_nodes.add_category('Camera', icon='OUTLINER_OB_CAMERA', section="data")
 arm_nodes.add_category('Material', icon='MATERIAL', section="data")
@@ -16,25 +16,25 @@ arm_nodes.add_category('Object', icon='OBJECT_DATA', section="data")
 arm_nodes.add_category('Scene', icon='SCENE_DATA', section="data")
 arm_nodes.add_category('Trait', icon='NODETREE', section="data")
 
-arm_nodes.add_category('Animation', icon='SEQUENCE', section="motion") # FRAME_NEXT | ONIONSKIN_ON
-arm_nodes.add_category('Navmesh', icon='UV_VERTEXSEL', section="motion") #TRACKING_FORWARDS
+arm_nodes.add_category('Animation', icon='SEQUENCE', section="motion")
+arm_nodes.add_category('Navmesh', icon='UV_VERTEXSEL', section="motion")
 arm_nodes.add_category('Transform', icon='TRANSFORM_ORIGINS', section="motion")
 arm_nodes.add_category('Physics', icon='PHYSICS', section="motion")
 
 arm_nodes.add_category('Array', icon='LIGHTPROBE_GRID', section="values")
-arm_nodes.add_category('Math', icon='FORCE_HARMONIC', section="values") #LINENUMBERS_ON | CON_TRANSFORM
-arm_nodes.add_category('Random', icon='SEQ_HISTOGRAM', section="values") #FORCE_BOID | OUTLINER_OB_FORCE_FIELD
-arm_nodes.add_category('String', icon='SORTALPHA', section="values") #SYNTAX_OFF | SORTSIZE | OUTLINER_OB_FONT | FILE_TEXT
-arm_nodes.add_category('Variable', icon='OPTIONS', section="values") #FILE_VOLUME
+arm_nodes.add_category('Math', icon='FORCE_HARMONIC', section="values")
+arm_nodes.add_category('Random', icon='SEQ_HISTOGRAM', section="values")
+arm_nodes.add_category('String', icon='SORTALPHA', section="values")
+arm_nodes.add_category('Variable', icon='OPTIONS', section="values")
 
-arm_nodes.add_category('Canvas', icon='RENDERLAYERS', section="graphics") #CON_SIZELIKE | IMAGE_RGB_ALPHA
+arm_nodes.add_category('Canvas', icon='RENDERLAYERS', section="graphics")
 arm_nodes.add_category('Postprocess', icon='FREEZE', section="graphics")
 arm_nodes.add_category('Renderpath', icon='STICKY_UVS_LOC', section="graphics")
 
 arm_nodes.add_category('Sound', icon='OUTLINER_OB_SPEAKER', section="sound")
 
 arm_nodes.add_category('Miscellaneous', icon='RESTRICT_COLOR_ON', section="misc")
-arm_nodes.add_category('Layout', icon='SEQ_STRIP_DUPLICATE', section="misc") # ANCHOR_LEFT | UV_ISLANDSEL
+arm_nodes.add_category('Layout', icon='SEQ_STRIP_DUPLICATE', section="misc")
 
 # Make sure that logic node extension packs are displayed at the end
 # of the menu by default unless they declare it otherwise
