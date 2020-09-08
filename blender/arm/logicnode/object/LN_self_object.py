ce3a4dd1a0dd0221188f3875f4cdbf80d334aaa6
Moritz Brückner <mail@moritz-brueckner.de>
2020-09-08 23:30:37 +0200

    SelfNode -> SelfObjectNode

---
+++ blender/arm/logicnode/object/LN_self_object.py
@@ -0,0 +1,12 @@
+from arm.logicnode.arm_nodes import *
+
+class SelfObjectNode(ArmLogicTreeNode):
+    """Returns the object that owns the current trait."""
+    bl_idname = 'LNSelfNode'
+    bl_label = 'Self Object'
+    bl_icon = 'NONE'
+
+    def init(self, context):
+        self.add_output('ArmNodeSocketObject', 'Object')
+
+add_node(SelfObjectNode, category=MODULE_AS_CATEGORY)
