dc00b6abd550c222443ead179bb32d320c4029d5
Moritz Brückner <mail@moritz-brueckner.de>
2020-09-08 23:31:09 +0200

    Fix ParseFloatNode label and add it to its own section

--- blender/arm/logicnode/string/LN_parse_float.py
+++ blender/arm/logicnode/string/LN_parse_float.py
@@ -3,11 +3,11 @@ from arm.logicnode.arm_nodes import *
 class ParseFloatNode(ArmLogicTreeNode):
     """Parse float node"""
     bl_idname = 'LNParseFloatNode'
-    bl_label = 'Parse float'
+    bl_label = 'Parse Float'
     bl_icon = 'NONE'
 
     def init(self, context):
         self.add_output('NodeSocketFloat', 'Float')
         self.add_input('NodeSocketString', 'String')
 
-add_node(ParseFloatNode, category=MODULE_AS_CATEGORY)
+add_node(ParseFloatNode, category=MODULE_AS_CATEGORY, section='parse')
