830f4c6d0ff6075c5e92428bd0727c36bac8f95a
knowledgenude <63247726+knowledgenude@users.noreply.github.com>
2020-09-09 16:48:36 -0300

    Update GetMeshCulledNode.hx

--- Sources/armory/logicnode/GetMeshCulledNode.hx
+++ Sources/armory/logicnode/GetMeshCulledNode.hx
@@ -11,7 +11,7 @@ class GetMeshCulledNode extends LogicNode {
 	override function get(from: Int): Dynamic {
 		var object: Object = inputs[0].get();
 
-		if (object == null) false;
+		if (object == null) return;
 
 		return object.culledMesh;
 	}
