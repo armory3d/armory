1bde147a234b2f3b40446747f6f86dba1e21b0da
Moritz Brückner <mail@moritz-brueckner.de>
2020-09-09 20:34:23 +0200

    AddCollectionnode -> CreateCollectionNode

--- blender/arm/logicnode/scene/LN_create_collection.py
+++ blender/arm/logicnode/scene/LN_create_collection.py
@@ -1,13 +1,13 @@
 from arm.logicnode.arm_nodes import *
 
-class AddGroupNode(ArmLogicTreeNode):
+class CreateCollectionNode(ArmLogicTreeNode):
     """Add Group node"""
     bl_idname = 'LNAddGroupNode'
-    bl_label = 'Add Collection'
+    bl_label = 'Create Collection'
 
     def init(self, context):
         self.add_input('ArmNodeSocketAction', 'In')
         self.add_input('NodeSocketString', 'Collection')
         self.add_output('ArmNodeSocketAction', 'Out')
 
-add_node(AddGroupNode, category=MODULE_AS_CATEGORY, section='collection')
+add_node(CreateCollectionNode, category=MODULE_AS_CATEGORY, section='collection')
