04475b53f5fc8d853f82ff6f0ad5fdebe6377f53
knowledgenude <63247726+knowledgenude@users.noreply.github.com>
2020-09-09 16:49:43 -0300

    Update IsRigidBodyNode.hx

--- Sources/armory/logicnode/IsRigidBodyNode.hx
+++ Sources/armory/logicnode/IsRigidBodyNode.hx
@@ -12,7 +12,7 @@ class IsRigidBodyNode extends LogicNode {
 	override function get(from: Int): Dynamic {
 		var object: Object = inputs[0].get();
 
-		if (object == null) false;
+		if (object == null) return;
 
 		#if arm_physics
 		var rigidBody = object.getTrait(RigidBody);
