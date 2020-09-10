3a3ec6ea253b212c2a5f0dda9cf1fae645aae82a
knowledgenude <63247726+knowledgenude@users.noreply.github.com>
2020-09-10 20:11:29 -0300

    Added Children Checkbox in Set Visible node .hx

--- Sources/armory/logicnode/SetVisibleNode.hx
+++ Sources/armory/logicnode/SetVisibleNode.hx
@@ -13,16 +13,30 @@ class SetVisibleNode extends LogicNode {
 	override function run(from: Int) {
 		var object: Object = inputs[1].get();
 		var visible: Bool = inputs[2].get();
+		var children: Bool = inputs[3].get();
 
 		if (object == null) return;
+
+		var objectChildren: Array<Object> = object.children;
 
 		switch (property0) {
 		case "Object":
 			object.visible = visible;
+			if (children == true) for (child in objectChildren) {
+        	child.visible = visible;
+        	}
+
 		case "Mesh":
 			object.visibleMesh = visible;
+			if (children == true) for (child in objectChildren) {
+			child.visibleMesh = visible;
+			}
+
 		case "Shadow":
 			object.visibleShadow = visible;
+			if (children == true) for (child in objectChildren) {
+			child.visibleShadow = visible;
+			}
 		}
 
 		runOutput(0);
