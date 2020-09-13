d44bf0f4bb09676aecbccb4faf178587ece1aebf
knowledgenude <63247726+knowledgenude@users.noreply.github.com>
2020-09-13 14:08:55 -0300

    Improved nodes +2 new nodes .hx

---
+++ Sources/armory/logicnode/GetRigidBodyDataNode.hx
@@ -0,0 +1,38 @@
+package armory.logicnode;
+
+import iron.object.Object;
+import armory.trait.physics.RigidBody;
+
+class GetRigidBodyDataNode extends LogicNode {
+
+	public function new(tree: LogicTree) {
+		super(tree);
+	}
+
+	override function get(from: Int): Dynamic {
+		var object: Object = inputs[0].get();
+
+		if (object == null) return null;
+
+		#if arm_physics
+		var rigidBody = object.getTrait(RigidBody);
+
+		return switch (from) {
+			case 0:
+			if (rigidBody == null) return false;
+			return true;
+
+			//case 1: rigidBody.btshape;
+			case 1: rigidBody.group;
+			case 2: rigidBody.mask;
+			case 3: rigidBody.animated;
+			case 4: rigidBody.staticObj;
+			case 5: rigidBody.angularDamping;
+			case 6: rigidBody.linearDamping;
+			case 7: rigidBody.friction;
+			case 8: rigidBody.mass;
+		default: null;
+		}
+		#end
+	}
+}
--- Sources/armory/logicnode/SetVisibleNode.hx
+++ Sources/armory/logicnode/SetVisibleNode.hx
@@ -4,6 +4,8 @@ import iron.object.Object;
 
 class SetVisibleNode extends LogicNode {
 
+	public var property0: String;
+
 	public function new(tree: LogicTree) {
 		super(tree);
 	}
@@ -11,10 +13,31 @@ class SetVisibleNode extends LogicNode {
 	override function run(from: Int) {
 		var object: Object = inputs[1].get();
 		var visible: Bool = inputs[2].get();
+		var children: Bool = inputs[3].get();
 
 		if (object == null) return;
 
-		object.visible = visible;
+		var objectChildren: Array<Object> = object.children;
+
+		switch (property0) {
+		case "Object":
+			object.visible = visible;
+			if (children == true) for (child in objectChildren) {
+        	child.visible = visible;
+        	}
+
+		case "Mesh":
+			object.visibleMesh = visible;
+			if (children == true) for (child in objectChildren) {
+			child.visibleMesh = visible;
+			}
+
+		case "Shadow":
+			object.visibleShadow = visible;
+			if (children == true) for (child in objectChildren) {
+			child.visibleShadow = visible;
+			}
+		}
 
 		runOutput(0);
 	}
