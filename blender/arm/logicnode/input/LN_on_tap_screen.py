9ac4325a005c1b5f21e3b4f3f57920ba828e4048
E1e5en <e1e5en@inbox.ru>
2020-09-18 08:37:44 +0300

    On Tap Screen - node for handling touches (double, triple…)
    Input parameters:
    
    Duration - time during which the number of touches is counted (default value is 0.3 seconds);
    Interval - the time that must elapse between touches (default value is 0);
    Repeat - the number of touches that need to be done to execute the event (default value is 2).
    Output parameters:
    
    Done - event for a successful completed action;
    Fail - an event for a failed action;
    Tap - event on each touch;
    Tap Number - how many times the user has already touched the screen;
    Coords - coordinates of the touch.

---
+++ blender/arm/logicnode/input/LN_on_tap_screen.py
@@ -0,0 +1,25 @@
+from arm.logicnode.arm_nodes import *
+
+# Class OnTapScreen
+class OnTapScreen(ArmLogicTreeNode):
+    """On Tap Screen Node"""
+    bl_idname = 'LNOnTapScreen'
+    bl_label = 'On Tap Screen'
+    arm_version = 1
+
+    def init(self, context):  
+        super(OnTapScreen, self).init(context)   
+        self.add_input('NodeSocketFloat', 'Duration')
+        self.inputs[-1].default_value = 0.3
+        self.add_input('NodeSocketFloat', 'Interval')
+        self.inputs[-1].default_value = 0.0
+        self.add_input('NodeSocketInt', 'Repeat')
+        self.inputs[-1].default_value = 2
+        self.add_output('ArmNodeSocketAction', 'Done')
+        self.add_output('ArmNodeSocketAction', 'Fail')
+        self.add_output('ArmNodeSocketAction', 'Tap')
+        self.add_output('NodeSocketInt', 'Tap Number')
+        self.add_output('NodeSocketVector', 'Coords')
+
+# Add Node
+add_node(OnTapScreen, category=PKG_AS_CATEGORY, section='Input')
