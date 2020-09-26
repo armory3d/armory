2c2ecee2a97b0bc79f7aaf005f5f872780d1cb83
Moritz Brückner <mail@moritz-brueckner.de>
2020-09-26 16:33:30 +0200

    GetObjectNode: allow getting objects from other scenes

--- Sources/armory/logicnode/GetObjectNode.hx
+++ Sources/armory/logicnode/GetObjectNode.hx
@@ -1,6 +1,12 @@
 package armory.logicnode;
 
+import iron.data.SceneFormat;
+import iron.object.Object;
+
 class GetObjectNode extends LogicNode {
+
+	/** Scene from which to take the object **/
+	public var property0: Null<String>;
 
 	public function new(tree: LogicTree) {
 		super(tree);
@@ -9,6 +15,35 @@ class GetObjectNode extends LogicNode {
 	override function get(from: Int): Dynamic {
 		var objectName: String = inputs[0].get();
 
-		return iron.Scene.active.getChild(objectName);
+		if (property0 == null || property0 == iron.Scene.active.raw.name) {
+			return iron.Scene.active.getChild(objectName);
+		}
+
+		#if arm_json
+		property0 += ".json";
+		#elseif arm_compress
+		property0 += ".lz4";
+		#end
+
+		var outObj: Null<Object> = null;
+
+		// Create the object in the active scene if it is from an inactive scene
+		iron.data.Data.getSceneRaw(property0, (rawScene: TSceneFormat) -> {
+			var objData: Null<TObj> = null;
+
+			for (o in rawScene.objects) {
+				if (o.name == objectName) {
+					objData = o;
+					break;
+				}
+			}
+			if (objData == null) return;
+
+			iron.Scene.active.createObject(objData, rawScene, null, null, (newObj: Object) -> {
+				outObj = newObj;
+			});
+		});
+
+		return outObj;
 	}
 }
