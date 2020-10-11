46f0c3eb9bb6c491b2c74a2c0dfd56316fb22487
knowledgenude <63247726+knowledgenude@users.noreply.github.com>
2020-10-11 12:05:44 -0300

    Update LN_play_action.py

--- blender/arm/logicnode/deprecated/LN_play_action.py
+++ blender/arm/logicnode/deprecated/LN_play_action.py
@@ -3,6 +3,7 @@ from arm.logicnode.arm_nodes import *
 class PlayActionNode(ArmLogicTreeNode):
     """Plays the given action."""
     bl_idname = 'LNPlayActionNode'
+    bl_label = 'Play Action'
     bl_description = "Please use the \"Play Action From\" node instead"
     bl_icon = 'ERROR'
     arm_version = 2
