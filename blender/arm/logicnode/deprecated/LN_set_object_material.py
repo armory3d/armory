5abe9849207e3eaa8e1aeb93a0dd21706e962e52
knowledgenude <63247726+knowledgenude@users.noreply.github.com>
2020-10-11 12:08:05 -0300

    Update LN_set_object_material.py

--- blender/arm/logicnode/deprecated/LN_set_object_material.py
+++ blender/arm/logicnode/deprecated/LN_set_object_material.py
@@ -3,6 +3,7 @@ from arm.logicnode.arm_nodes import *
 class SetMaterialNode(ArmLogicTreeNode):
     """Sets the material of the given object."""
     bl_idname = 'LNSetMaterialNode'
+    bl_label = 'Set Object Material'
     bl_description = "Please use the \"Set Object Material Slot\" node instead"
     bl_icon = 'ERROR'
     arm_version = 2
