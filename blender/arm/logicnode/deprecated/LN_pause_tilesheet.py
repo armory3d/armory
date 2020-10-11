e5534c2998ff94e3afe947088bdc943d607d5c1a
knowledgenude <63247726+knowledgenude@users.noreply.github.com>
2020-10-11 11:56:17 -0300

    Update LN_pause_tilesheet.py

--- blender/arm/logicnode/deprecated/LN_pause_tilesheet.py
+++ blender/arm/logicnode/deprecated/LN_pause_tilesheet.py
@@ -3,6 +3,7 @@ from arm.logicnode.arm_nodes import *
 class PauseTilesheetNode(ArmLogicTreeNode):
     """Pauses the given tilesheet action."""
     bl_idname = 'LNPauseTilesheetNode'
+    bl_label = 'Pause Tilesheet'
     bl_description = "Please use the \"Set Tilesheet Paused\" node instead"
     bl_icon = 'ERROR'
     arm_version = 2
