6ed63ad7d0f8f0c2ee735be47de44e61dd79e56f
knowledgenude <63247726+knowledgenude@users.noreply.github.com>
2020-10-11 12:06:36 -0300

    Update LN_resume_action.py

--- blender/arm/logicnode/deprecated/LN_resume_action.py
+++ blender/arm/logicnode/deprecated/LN_resume_action.py
@@ -3,6 +3,7 @@ from arm.logicnode.arm_nodes import *
 class ResumeActionNode(ArmLogicTreeNode):
     """Resumes the given action."""
     bl_idname = 'LNResumeActionNode'
+    bl_label = 'Resume Action'
     bl_description = "Please use the \"Set Action Paused\" node instead"
     bl_icon = 'ERROR'
     arm_version = 2
