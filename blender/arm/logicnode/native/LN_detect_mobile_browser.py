145686dbf46dfc6f2b76880dbb015bb83ed7eed5
E1e5en <doksergey@gmail.com>
2020-10-30 21:22:52 +0300

    LN Detect Mobile Browser
    Detect Mobile Browser

---
+++ blender/arm/logicnode/native/LN_detect_mobile_browser.py
@@ -0,0 +1,11 @@
+from arm.logicnode.arm_nodes import *
+
+class DetectMobileBrowserNode(ArmLogicTreeNode):
+	"""Determines the mobile browser or not (works only for web browsers)."""
+	bl_idname = 'LNDetectMobileBrowserNode'
+	bl_label = 'Detect Mobile Browser'
+	arm_version = 1
+
+	def init(self, context):
+		super(DetectMobileBrowserNode, self).init(context)
+		self.add_output('NodeSocketBool', 'Mobile')
