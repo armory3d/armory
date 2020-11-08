fde59db1e5b5b849cde42627a3affb4fc31e96a7
knowledgenude <phenriquefel@gmail.com>
2020-11-08 17:37:50 -0300

    value-changed-node

---
+++ blender/arm/logicnode/logic/LN_value_changed.py
@@ -0,0 +1,16 @@
+from arm.logicnode.arm_nodes import *
+
+class ValueChangedNode(ArmLogicTreeNode):
+    """Sends a signal to `Changed` output whether the connected value is changed and a signal to `Unchanged` if the connected value returns to initial. Does not works with `null` value."""
+    bl_idname = 'LNValueChangedNode'
+    bl_label = 'Value Changed'
+    arm_version = 1
+
+    def init(self, context):
+        super(ValueChangedNode, self).init(context)
+        self.add_input('ArmNodeSocketAction', 'In')
+        self.add_input('NodeSocketShader', 'Value')
+        self.add_output('ArmNodeSocketAction', 'Changed')
+        self.add_output('ArmNodeSocketAction', 'Unchanged')
+
+add_node(ValueChangedNode, category=PKG_AS_CATEGORY)
