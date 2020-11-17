14375b7fada46edd41b91342a6b4fc04782c70ba
Moritz Brückner <mail@moritz-brueckner.de>
2020-11-17 11:59:21 +0100

    Cleanup driver API code and add docstring + type info

--- blender/arm/api.py
+++ blender/arm/api.py
@@ -1,16 +1,30 @@
+from typing import Callable, Dict, Optional
 
-drivers = dict()
+from bpy.types import UILayout
 
-def add_driver(driver_name, draw_props, make_rpass, make_rpath):
-	global drivers
+from arm.material.shader import ShaderContext
 
-	if driver_name in drivers:
-		return
+drivers: Dict[str, Dict] = dict()
 
-	d = {}
-	d['driver_name'] = driver_name
-	d['draw_props'] = draw_props
-	d['make_rpass'] = make_rpass
-	d['make_rpath'] = make_rpath
 
-	drivers[driver_name] = d
+def add_driver(driver_name: str,
+               draw_props: Callable[[UILayout], None],
+               make_rpass: Callable[[str], Optional[ShaderContext]], make_rpath: Callable[[], None]) -> None:
+    """Register a new driver. If there already exists a driver with the given name, nothing happens.
+
+    @param driver_name Unique name for the new driver
+    @param draw_props Function to draw global driver properties inside the render path panel
+    @param make_rpass Function to create render passes. Takes the rpass name as a parameter and may return `None`.
+    @param make_rpath Function to setup the render path.
+    """
+    global drivers
+
+    if driver_name in drivers:
+        return
+
+    drivers[driver_name] = {
+        'driver_name': driver_name,
+        'draw_props': draw_props,
+        'make_rpass': make_rpass,
+        'make_rpath': make_rpath
+    }
