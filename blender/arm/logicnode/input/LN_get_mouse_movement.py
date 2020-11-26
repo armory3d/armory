0acadd2e74331a31509d840bc65d74897b462f8a
Moritz Brückner <mail@moritz-brueckner.de>
2020-11-26 10:00:35 +0100

    Improve `Get Mouse Movement` node

--- blender/arm/logicnode/input/LN_get_mouse_movement.py
+++ blender/arm/logicnode/input/LN_get_mouse_movement.py
@@ -1,7 +1,9 @@
 from arm.logicnode.arm_nodes import *
 
+
 class GetMouseMovementNode(ArmLogicTreeNode):
-    """Returns the movement coordinates of the mouse."""
+    """Get the movement coordinates of the mouse and the mouse wheel delta.
+    The multiplied output variants default to -1 to invert the values."""
     bl_idname = 'LNGetMouseMovementNode'
     bl_label = 'Get Mouse Movement'
     arm_section = 'mouse'
@@ -9,13 +11,14 @@ class GetMouseMovementNode(ArmLogicTreeNode):
 
     def init(self, context):
         super(GetMouseMovementNode, self).init(context)
-        self.add_input('NodeSocketFloat', 'X Multiplier' , default_value=-1.0)
+
+        self.add_input('NodeSocketFloat', 'X Multiplier', default_value=-1.0)
         self.add_input('NodeSocketFloat', 'Y Multiplier', default_value=-1.0)
-        self.add_input('NodeSocketFloat', 'Delta Multiplier', default_value=-1.0)
+        self.add_input('NodeSocketFloat', 'Wheel Delta Multiplier', default_value=-1.0)
 
         self.add_output('NodeSocketFloat', 'X')
         self.add_output('NodeSocketFloat', 'Y')
-        self.add_output('NodeSocketInt', 'Delta')
         self.add_output('NodeSocketFloat', 'Multiplied X')
         self.add_output('NodeSocketFloat', 'Multiplied Y')
-        self.add_output('NodeSocketFloat', 'Multiplied Delta')
+        self.add_output('NodeSocketInt', 'Wheel Delta')
+        self.add_output('NodeSocketFloat', 'Multiplied Wheel Delta')
