d18d9e1a2776f93c27e09657d98b66a90b4b49d1
Moritz Brückner <mail@moritz-brueckner.de>
2020-11-29 17:16:43 +0100

    Fix copy_pass missing on deferred when only compositor was enabled

--- blender/arm/make_renderpath.py
+++ blender/arm/make_renderpath.py
@@ -144,7 +144,7 @@ def build():
             assets.add(assets_path + 'clouds_map.png')
             assets.add_embedded_data('clouds_map.png')
 
-    if rpdat.rp_renderer == 'Deferred' and not rpdat.rp_compositornodes:
+    if rpdat.rp_renderer == 'Deferred':
         assets.add_shader_pass('copy_pass')
 
     if rpdat.rp_render_to_texture:
