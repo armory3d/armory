826c3b10d2ecfd380890a2d39ba46b039c75159d
onelsonic <53947594+onelsonic@users.noreply.github.com>
2021-04-08 14:07:50 +0200

    Correclty detecting the system
    I was unable to compile the API documentation on windows (https://github.com/armory3d/armsdk/tree/master/api)
    until editing this node.
    Need to check if this does not break anything else.

--- Sources/armory/logicnode/GetSystemName.hx
+++ Sources/armory/logicnode/GetSystemName.hx
@@ -11,12 +11,11 @@ class GetSystemName extends LogicNode {
 	override function get(from: Int): Dynamic {
 
 		return switch (from) {
-			case 0: systemName;
-			case 1: equalsCI(kha.System.systemId, 'Windows');
-			case 2: equalsCI(kha.System.systemId, 'Linux');
-			case 3: equalsCI(kha.System.systemId, 'Mac');
-			case 4: equalsCI(kha.System.systemId, 'HTML5');
-			case 5: equalsCI(kha.System.systemId, 'Android');
+			case 0: equalsCI(kha.System.systemId, 'Windows');
+			case 1: equalsCI(kha.System.systemId, 'Linux');
+			case 2: equalsCI(kha.System.systemId, 'Mac');
+			case 3: equalsCI(kha.System.systemId, 'HTML5');
+			case 4: equalsCI(kha.System.systemId, 'Android');
 			default: null;
 		}
 
