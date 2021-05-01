1ebe5284174e2d3d31acd3b37ce436cf70a28be2
QuantumCoderQC <quantumcoderqc@gmail.com>
2021-05-02 01:30:34 +0200

    Remove scene port from logic node

--- blender/arm/logicnode/object/LN_get_object_by_name.py
+++ blender/arm/logicnode/object/LN_get_object_by_name.py
@@ -5,19 +5,13 @@ from arm.logicnode.arm_nodes import *
 
 class GetObjectNode(ArmLogicTreeNode):
     """Searches for a object that uses the given name in the current active scene and returns it."""
+    
     bl_idname = 'LNGetObjectNode'
     bl_label = 'Get Object by Name'
     arm_version = 1
-
-    property0: PointerProperty(
-        type=bpy.types.Scene, name='Scene',
-        description='The scene from which to take the object')
 
     def init(self, context):
         super(GetObjectNode, self).init(context)
         self.add_input('NodeSocketString', 'Name')
 
         self.add_output('ArmNodeSocketObject', 'Object')
-
-    def draw_buttons(self, context, layout):
-        layout.prop_search(self, 'property0', bpy.data, "scenes")
