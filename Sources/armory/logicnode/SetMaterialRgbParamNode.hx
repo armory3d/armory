eab7fcbbe6ad54b6ca826ff0d044b6f9e590840b
QuantumCoderQC <quantumcoderqc@gmail.com>
2021-06-14 13:37:10 +0200

    update node implementations

--- Sources/armory/logicnode/SetMaterialRgbParamNode.hx
+++ Sources/armory/logicnode/SetMaterialRgbParamNode.hx
@@ -4,32 +4,30 @@ import iron.Scene;
 import iron.math.Vec4;
 import iron.data.MaterialData;
 import iron.object.Object;
-import armory.trait.internal.UniformsManager;
+import iron.data.UniformsManager;
 
 class SetMaterialRgbParamNode extends LogicNode {
-
-	public var property0: Bool; // per object
 	
-	var manager: UniformsManager;
-
 	public function new(tree: LogicTree) {
 		super(tree);
-
-		manager = new UniformsManager(UniformType.Vector);
 	}
 
 	override function run(from: Int) {
 		var object = inputs[1].get();
 		if(object == null) return;
 
-		var mat = inputs[2].get();
+		var perObject = inputs[2].get();
+		if(perObject == null) perObject = false;
+
+		var mat = inputs[3].get();
 		if(mat == null) return;
 
-		if(! property0){
+		if(! perObject){
+			UniformsManager.removeObjectFromMap(object, Vector);
 			object = Scene.active.root;
 		}
 
-		manager.setVec3Value(mat, object, inputs[3].get(), inputs[4].get());
+		UniformsManager.setVec3Value(mat, object, inputs[4].get(), inputs[5].get());
 		runOutput(0);
 	}
 }
