daee309ad851c761f52c186fd1caa40c70553af0
Henrique <phenriquefel@gmail.com>
2021-07-07 17:08:01 -0300

    Add Remove Input Map Key node

---
+++ Sources/armory/logicnode/RemoveInputMapKeyNode.hx
@@ -0,0 +1,23 @@
+package armory.logicnode;
+
+import armory.system.InputMap;
+
+class RemoveInputMapKeyNode extends LogicNode {
+
+	public function new(tree: LogicTree) {
+		super(tree);
+	}
+
+	override function run(from: Int) {
+		var inputMap = inputs[1].get();
+		var key = inputs[2].get();
+
+		var k = InputMap.getInputMapKey(inputMap, key);
+
+		if (k != null) {
+			if (InputMap.getInputMap(inputMap).removeInput(k)) {
+				runOutput(0);
+			}
+		}
+	}
+}
