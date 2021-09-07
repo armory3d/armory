d54848ab0787094674b0c6dfe72708a27f2c9544
QuantumCoderQC <QuantumCoderQC@gmail.com>
2021-09-07 12:30:43 +0200

    modify action node

--- Sources/armory/logicnode/AnimActionNode.hx
+++ Sources/armory/logicnode/AnimActionNode.hx
@@ -1,21 +1,42 @@
 package armory.logicnode;
 
+import iron.object.Object;
+import iron.object.Animation;
+import iron.object.BoneAnimation;
+import iron.math.Mat4;
+
+
 class AnimActionNode extends LogicNode {
 
+	public var property0: String;
 	public var value: String;
+	public var actionParam: Animparams;
+	var object: Object;
+	var animation: Animation;
 
-	public function new(tree: LogicTree, value = "") {
+	public function new(tree: LogicTree) {
 		super(tree);
-		this.value = value;
+		tree.notifyOnInit(init);
+
+	}
+
+	function init(){
+		actionParam = new Animparams(inputs[1].get(), 1.0, inputs[2].get());
+		object = inputs[0].get();
+		if (object == null) return;
+		animation = object.animation;
+		if (animation == null) animation = object.getParentArmature(object.name);
+		animation.registerAction(property0, actionParam);
+
 	}
 
 	override function get(from: Int): Dynamic {
-		if (inputs.length > 0) return inputs[0].get();
-		return value;
+		return function (animMats: Array<Mat4>){ animation.sampleAction(actionParam, animMats);};
+		
 	}
 
-	override function set(value: Dynamic) {
+	/* override function set(value: Dynamic) {
 		if (inputs.length > 0) inputs[0].set(value);
 		else this.value = value;
-	}
+	} */
 }
--- blender/arm/logicnode/animation/LN_action.py
+++ blender/arm/logicnode/animation/LN_action.py
@@ -1,12 +1,22 @@
 from arm.logicnode.arm_nodes import *
 
 class AnimActionNode(ArmLogicTreeNode):
-    """Stores the given action as a variable."""
+    """Samples a given action."""
     bl_idname = 'LNAnimActionNode'
     bl_label = 'Action'
-    arm_version = 1
+    arm_version = 2
+
+    property0: HaxeStringProperty('property0', name = 'Action ID', default = '')
 
     def arm_init(self, context):
+        self.add_input('ArmNodeSocketObject', 'Object')
         self.add_input('ArmNodeSocketAnimAction', 'Action')
+        self.add_input('ArmBoolSocket', 'Is Looped')
 
-        self.add_output('ArmNodeSocketAnimAction', 'Action', is_var=True)
+        self.add_output('ArmNodeSocketAnimTree', 'Action')
+    
+    def draw_buttons(self, context, layout):
+        layout.prop(self, 'property0')
+    
+    def draw_label(self) -> str:
+        return f'{self.bl_label}: {self.property0}'
