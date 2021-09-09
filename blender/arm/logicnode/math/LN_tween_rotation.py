62294c8bb4db24f10cae894bbc82b819d3183b73
QuantumCoderQC <QuantumCoderQC@gmail.com>
2021-09-10 01:08:12 +0200

    create tween rotation node

---
+++ blender/arm/logicnode/math/LN_tween_rotation.py
@@ -0,0 +1,54 @@
+from arm.logicnode.arm_nodes import *
+
+class TweenFloatNode( ArmLogicTreeNode):
+    '''Tween rotation'''
+    bl_idname = 'LNTweenRotationNode'
+    bl_label = 'Tween Rotation'
+    arm_version = 1
+    
+    property0: HaxeEnumProperty(
+        'property0',
+        items = [('Linear', 'Linear', 'Linear'),
+                ('SineIn', 'SineIn', 'SineIn'),
+                ('SineOut', 'SineOut', 'SineOut'),
+                ('SineInOut', 'SineInOut', 'SineInOut'),
+                ('QuadIn', 'QuadIn', 'QuadIn'),
+                ('QuadOut', 'QuadOut', 'QuadOut'),
+                ('QuadInOut', 'QuadInOut', 'QuadInOut'),
+                ('CubicIn', 'CubicIn', 'CubicIn'),
+                ('CubicOut', 'CubicOut', 'CubicOut'),
+                ('CubicInOut', 'CubicInOut', 'CubicInOut'),
+                ('QuartIn', 'QuartIn', 'QuartIn'),
+                ('QuartOut', 'QuartOut', 'QuartOut'),
+                ('QuartInOut', 'QuartInOut', 'QuartInOut'),
+                ('QuintIn', 'QuintIn', 'QuintIn'),
+                ('QuintOut', 'QuintOut', 'QuintOut'),
+                ('QuintInOut', 'QuintInOut', 'QuintInOut'),
+                ('ExpoIn', 'ExpoIn', 'ExpoIn'),
+                ('ExpoOut', 'ExpoOut', 'ExpoOut'),
+                ('ExpoInOut', 'ExpoInOut', 'ExpoInOut'),
+                ('CircIn', 'CircIn', 'CircIn'),
+                ('CircOut', 'CircOut', 'CircOut'),
+                ('CircInOut', 'CircInOut', 'CircInOut'),
+                ('BackIn', 'BackIn', 'BackIn'),
+                ('BackOut', 'BackOut', 'BackOut'),
+                ('BackInOut', 'BackInOut', 'BackInOut')],
+        name='', default='Linear')
+
+    def init(self, context):
+        self.add_input('ArmNodeSocketAction', 'Start')
+        self.add_input('ArmNodeSocketAction', 'Stop')
+        self.add_input('ArmRotationSocket', 'From')
+        self.add_input('ArmRotationSocket', 'To')
+        self.add_input('ArmFloatSocket', 'Time', default_value = 1.0)
+        
+        self.add_output('ArmRotationSocket', 'Tween')
+        self.add_output('ArmNodeSocketAction', 'Out')
+        self.add_output('ArmNodeSocketAction', 'Tick')
+        self.add_output('ArmNodeSocketAction', 'Done')
+
+    def draw_buttons(self, context, layout):
+        layout.prop(self, 'property0')
+    
+    def draw_label(self) -> str:
+        return f'{self.bl_label}: {self.property0}'
