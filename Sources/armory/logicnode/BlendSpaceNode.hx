b48b152a74ce8966f411ef965718e34e8d9ac0f7
QuantumCoderQC <QuantumCoderQC@gmail.com>
2021-11-22 22:13:27 +0100

    blend space node test

--- Sources/armory/logicnode/BlendSpaceNode.hx
+++ Sources/armory/logicnode/BlendSpaceNode.hx
@@ -1,10 +1,11 @@
 package armory.logicnode;
 
+import iron.math.Vec2;
 import haxe.ds.Vector;
 
 class BlendSpaceNode extends LogicNode {
 
-	public var property0: Array<Bool>;
+	public var property0: Array<Float>;
 	var value: Dynamic;
 	var index: Int;
 
@@ -14,7 +15,26 @@ class BlendSpaceNode extends LogicNode {
 
 	override function get(from: Int): Dynamic {
 
-		return property0;
+		var dist = [];
+		var vecs = [];
+		for(i in 0...property0.length % 2){
 
+			vecs.push(new Vec2(property0[i * 2], property0[i * 2 + 1]));
+		}
+
+		for (i in 0...vecs.length - 1){
+			dist.push(Vec2.distance(vecs[i], vecs[vecs.length]));
+		}
+
+		var distSort = dist.copy();
+		distSort.sort((a, b) -> b - a);
+
+		var index1 = distSort.indexOf(distSort[0]);
+		var index2 = distSort.indexOf(distSort[1]);
+		var index3 = distSort.indexOf(distSort[2]);
+
+		trace("1 : " + Std.string(index1) + " , " + Std.string(index2) + " , " + Std.string(index3));
+
+		return 0;
 	}
 }
