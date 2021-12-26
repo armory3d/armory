ed09afcb1d5c40a5f924d09aaa206e8c668a1d20
QuantumCoderQC <QuantumCoderQC@gmail.com>
2021-12-26 17:18:29 +0100

    implement float delta interpolation node

---
+++ Sources/armory/logicnode/FloatDeltaInterpolateNode.hx
@@ -0,0 +1,23 @@
+package armory.logicnode;
+
+import kha.FastFloat;
+
+class FloatDeltaInterpolateNode extends LogicNode {
+
+	public function new(tree: LogicTree) {
+		super(tree);
+		
+	}
+
+	override function get(from: Int): FastFloat {
+		var fromValue = inputs[0].get();
+		var toValue = inputs[1].get();
+		var deltaTime = inputs[2].get();
+		var rate = inputs[3].get();
+		
+		var value = fromValue + deltaTime * rate;
+		var min = Math.min(fromValue, toValue);
+		var max = Math.max(fromValue, toValue);
+		return value < min ? min : value > max ? max : value;
+	}
+}
