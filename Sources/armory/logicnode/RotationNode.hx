965cf5f9b624002e982ab4efdf87894ef01516ff
Moritz Brückner <mail@moritz-brueckner.de>
2022-07-03 17:28:33 +0200

    Use Helper rad/deg conversion functions

--- Sources/armory/logicnode/RotationNode.hx
+++ Sources/armory/logicnode/RotationNode.hx
@@ -1,12 +1,11 @@
 package armory.logicnode;
 
+import armory.math.Helper;
 import iron.math.Vec4;
 import iron.math.Quat;
 import kha.FastFloat;
 
 class RotationNode extends LogicNode {
-
-	static inline var toRAD: FastFloat = 0.017453292519943295;  // 180/pi
 
 	public var property0: String;  // type of input (EulerAngles, AxisAngle, Quaternion)
 	public var property1: String;  // angle unit (Deg, Rad)
@@ -43,16 +42,16 @@ class RotationNode extends LogicNode {
 				var vec: Vec4 = inputs[0].get();
 				var angle: FastFloat = inputs[1].get();
 				if (property1 == "Deg") {
-					angle *= toRAD;
+					angle = Helper.degToRad(angle);
 				}
 				value.fromAxisAngle(vec, angle);
 
 			case "EulerAngles":
 				var vec: Vec4 = new Vec4().setFrom(inputs[0].get());
 				if (property1 == "Deg") {
-					vec.x *= toRAD;
-					vec.y *= toRAD;
-					vec.z *= toRAD;
+					vec.x = Helper.degToRad(vec.x);
+					vec.y = Helper.degToRad(vec.y);
+					vec.z = Helper.degToRad(vec.z);
 				}
 				this.value.fromEulerOrdered(vec, property2);
 
@@ -81,7 +80,7 @@ class RotationNode extends LogicNode {
 				var vec = new Vec4();
 				var angle = this.value.toAxisAngle(vec);
 				if (property1 == "Deg") {
-					angle /= toRAD;
+					angle = Helper.radToDeg(angle);
 				}
 				inputs[0].set(vec);
 				inputs[1].set(angle);
@@ -89,9 +88,9 @@ class RotationNode extends LogicNode {
 			case "EulerAngles":
 				var vec: Vec4 = value.toEulerOrdered(property2);
 				if (property1 == "Deg") {
-					vec.x /= toRAD;
-					vec.y /= toRAD;
-					vec.z /= toRAD;
+					vec.x = Helper.radToDeg(vec.x);
+					vec.y = Helper.radToDeg(vec.y);
+					vec.z = Helper.radToDeg(vec.z);
 				}
 				inputs[0].set(vec);
 
