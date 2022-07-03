2f5a5cff72d9d22d44a746e141280d14355b6bbe
yvain <yvain29@gmail.com>
2022-07-03 14:43:57 +0200

    squash! Use new tools path

--- Sources/armory/trait/FirstPersonController.hx
+++ Sources/armory/trait/FirstPersonController.hx
@@ -1,4 +1,4 @@
-package armory.trait;
+package arm;
 
 import iron.math.Vec4;
 import iron.system.Input;
@@ -13,7 +13,7 @@ class FirstPersonController extends CameraController {
 	public function new() { super(); }
 #else
 
-	var head: Object;
+	var head : Object;
 	static inline var rotationSpeed = 2.0;
 
 	public function new() {
@@ -24,7 +24,6 @@ class FirstPersonController extends CameraController {
 
 	function init() {
 		head = object.getChildOfType(CameraObject);
-
 		PhysicsWorld.active.notifyOnPreUpdate(preUpdate);
 		notifyOnUpdate(update);
 		notifyOnRemove(removed);
@@ -52,7 +51,12 @@ class FirstPersonController extends CameraController {
 		PhysicsWorld.active.removePreUpdate(preUpdate);
 	}
 
-	var dir = new Vec4();
+    var dir = new Vec4();
+	var add1 = new Vec4();
+	var add2 = new Vec4();
+	var add = new Vec4();
+	var sin = 0.0;
+	var cos = 0.0;
 	function update() {
 		if (!body.ready) return;
 
@@ -63,21 +67,72 @@ class FirstPersonController extends CameraController {
 
 		// Move
 		dir.set(0, 0, 0);
-		if (moveForward) dir.add(transform.look());
-		if (moveBackward) dir.add(transform.look().mult(-1));
-		if (moveLeft) dir.add(transform.right().mult(-1));
-		if (moveRight) dir.add(transform.right());
+
+        var pi = 355/113;
+        var z = transform.rot.z;
+        var zToRad = z * pi / 180;
+
+        if (z >= 0 && z <= 90) {
+            cos = Math.cos(zToRad);
+            sin = Math.sin(zToRad);
+        }
+        else if (z > 90 && z <= 180) {
+            cos = -Math.cos(zToRad);
+            sin = Math.sin(zToRad);
+        }
+        else if (z > 180 && z <= 270) {
+            cos = -Math.cos(zToRad);
+            sin = -Math.sin(zToRad);
+        }
+        else if (z > 280 && z <= 360) {
+            cos = Math.cos(zToRad);
+            sin = -Math.sin(zToRad);
+        }
+
+        if (moveForward && moveLeft) {
+            add1.set(transform.look().x * cos, transform.look().y * cos, 0, 0);
+            add2.set(transform.right().mult(-1).x * sin, transform.right().mult(-1).y * sin, 0, 0);
+            add.add(add1.normalize());
+            add.add(add2.normalize());
+        }
+        else if (moveForward && moveRight) {
+            add1.set(transform.look().x * cos, transform.look().y * cos, 0, 0);
+            add2.set(transform.right().x * sin, transform.right().y * sin, 0, 0);
+            add.add(add1.normalize());
+            add.add(add2.normalize());
+        }
+        else if (moveBackward && moveLeft) {
+            add1.set(transform.look().mult(-1).x * cos, transform.look().mult(-1).y * cos, 0, 0);
+            add2.set(transform.right().mult(-1).x * sin, transform.right().mult(-1).y * sin, 0, 0);
+            add.add(add1.normalize());
+            add.add(add2.normalize());
+        }
+        else if (moveBackward && moveRight) {
+            add1.set(transform.look().mult(-1).x * cos, transform.look().mult(-1).y * cos, 0, 0);
+            add2.set(transform.right().x * sin, transform.right().y * sin, 0, 0);
+            add.add(add1.normalize());
+            add.add(add2.normalize());
+        }
+        else if (moveForward)
+            add.set(transform.look().x, transform.look().y, 0, 0);
+        else if (moveBackward)
+            add.set(transform.look().mult(-1).x, transform.look().mult(-1).y, 0, 0);
+        else if (moveLeft)
+            add.set(transform.right().mult(-1).x, transform.right().mult(-1).y, 0, 0);
+        else if (moveRight)
+            add.set(transform.right().x, transform.right().y, 0, 0);
+
+        dir.add(add.normalize());
 
 		// Push down
 		var btvec = body.getLinearVelocity();
 		body.setLinearVelocity(0.0, 0.0, btvec.z - 1.0);
 
 		if (moveForward || moveBackward || moveLeft || moveRight) {
-			dir.mult(6);
+			dir.mult(5);
 			body.activate();
 			body.setLinearVelocity(dir.x, dir.y, btvec.z - 1.0);
 		}
-
 		// Keep vertical
 		body.setAngularFactor(0, 0, 0);
 		camera.buildMatrix();
