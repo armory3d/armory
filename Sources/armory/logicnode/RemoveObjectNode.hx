ae9ec10a7f73b62fb4cf20926ed4d58600f34131
RPaladin <69180012+rpaladin@users.noreply.github.com>
2022-09-19 14:41:27 -0700

    Implement #2570

--- Sources/armory/logicnode/RemoveObjectNode.hx
+++ Sources/armory/logicnode/RemoveObjectNode.hx
@@ -1,6 +1,7 @@
 package armory.logicnode;
 
 import iron.object.Object;
+import armory.trait.physics.RigidBody;
 
 class RemoveObjectNode extends LogicNode {
 
@@ -10,11 +11,22 @@ class RemoveObjectNode extends LogicNode {
 
 	override function run(from: Int) {
 		var object: Object = inputs[1].get();
+		var removeChildren: Bool = inputs[2].get();
+		var keepChildrenTransforms: Bool = inputs[3].get();
 
 		if (object == null) return;
-
+		
+		if (removeChildren == false) {
+			for (c in object.children.copy()) {
+				c.setParent(iron.Scene.active.root, false, keepChildrenTransforms);
+											
+				#if arm_physics
+				var rigidBody = c.getTrait(RigidBody);
+				if (rigidBody != null) rigidBody.syncTransform();
+				#end
+			}
+		}
 		object.remove();
-
 		runOutput(0);
 	}
 }
