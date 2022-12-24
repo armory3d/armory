9f7d8d748de92fa41bab97a1275fdf0b77e77f6f
QuantumCoderQC <QuantumCoderQC@gmail.com>
2022-12-24 23:33:50 +0530

    Implement TouchInRegion

---
+++ blender/arm/logicnode/input/LN_touch_in_region.py
@@ -0,0 +1,37 @@
+from arm.logicnode.arm_nodes import *
+
+class TouchInRegionNode(ArmLogicTreeNode):
+    """Detect touch in specific region.
+
+    @input Center X/Y: The position of the center in pixels.
+    @input Width: Width of the region in pixels.
+    @input Height: Height of the region in pixels.
+    @input Angle: Rotation angle in radians. Rotation is clockwise.
+
+    @output On Enter: Activated after the cursor enters the region.
+    @output On Exit: Activated after the cursor exits the region.
+    @output Is Inside: True if inside the region. False otherwise.
+    """
+    bl_idname = 'LNTouchInRegionNode'
+    bl_label = 'Touch In Region'
+    arm_section = 'surface'
+    arm_version = 1
+
+    property0: HaxeEnumProperty(
+        'property0',
+        items = [('rectangle', 'Rectangle', 'Rectangular region'),
+                 ('ellipse', 'Ellipse', 'Elliptical or Circular region')],
+        name='', default='rectangle')
+
+    def arm_init(self, context):
+        self.add_input('ArmFloatSocket', 'Center X')
+        self.add_input('ArmFloatSocket', 'Center Y')
+        self.add_input('ArmFloatSocket', 'Width')
+        self.add_input('ArmFloatSocket', 'Height')
+        self.add_input('ArmFloatSocket', 'Angle')
+        self.add_output('ArmNodeSocketAction', 'On Enter')
+        self.add_output('ArmNodeSocketAction', 'On Exit')
+        self.add_output('ArmBoolSocket', 'Is Inside')
+
+    def draw_buttons(self, context, layout):
+        layout.prop(self, 'property0')
