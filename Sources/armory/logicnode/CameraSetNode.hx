3d26ede256dc976fb9b240295db1f9e5699c0471
Moritz Brückner <17685000+MoritzBrueckner@users.noreply.github.com>
2023-01-02 13:57:29 +0100

    Whitespace cleanup

--- Sources/armory/logicnode/CameraSetNode.hx
+++ Sources/armory/logicnode/CameraSetNode.hx
@@ -7,11 +7,10 @@ class CameraSetNode extends LogicNode {
 	}
 
 	override function run(from:Int) {
-
-        armory.renderpath.Postprocess.camera_uniforms[0] = inputs[1].get();
-        armory.renderpath.Postprocess.camera_uniforms[1] = inputs[2].get();
-        armory.renderpath.Postprocess.camera_uniforms[2] = inputs[3].get();
-        armory.renderpath.Postprocess.camera_uniforms[3] = inputs[4].get();
+		armory.renderpath.Postprocess.camera_uniforms[0] = inputs[1].get();
+		armory.renderpath.Postprocess.camera_uniforms[1] = inputs[2].get();
+		armory.renderpath.Postprocess.camera_uniforms[2] = inputs[3].get();
+		armory.renderpath.Postprocess.camera_uniforms[3] = inputs[4].get();
 		armory.renderpath.Postprocess.camera_uniforms[4] = inputs[5].get();
 		armory.renderpath.Postprocess.camera_uniforms[5] = inputs[6].get();
 		armory.renderpath.Postprocess.camera_uniforms[6] = inputs[7].get();
