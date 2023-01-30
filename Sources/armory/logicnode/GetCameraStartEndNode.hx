e56459a5c60b4b7ee4461fb5aa291067e47d977a
t3du <32546729+t3du@users.noreply.github.com>
2023-01-30 07:42:25 -0300

    update property0 logic

--- Sources/armory/logicnode/GetCameraStartEndNode.hx
+++ Sources/armory/logicnode/GetCameraStartEndNode.hx
@@ -2,17 +2,38 @@ package armory.logicnode;
 
 import iron.object.CameraObject;
 
-class GetCameraStartEndNode extends LogicNode {
+class SetCameraStartEndNode extends LogicNode {
+
+	public var property0: String;
 
 	public function new(tree: LogicTree) {
 		super(tree);
 	}
 
-	override function get(from: Int): Dynamic {
-		var camera: CameraObject = inputs[0].get();
-
-		if (camera == null) return null;
+	override function run(from: Int) {
+		var camera: CameraObject = inputs[1].get();
 		
-		return from == 0 ? camera.data.raw.near_plane : camera.data.raw.far_plane;
+		if (camera == null) return;
+		
+		if (property0 == 'Start'){
+			var start: Float = inputs[2].get();
+			camera.data.raw.near_plane = start;
+		}
+		else if (property0 == 'End' ){
+			var end: Float = inputs[2].get();
+			camera.data.raw.far_plane = end;
+		}
+		else {
+		
+		var start: Float = inputs[2].get();
+		camera.data.raw.near_plane = start;
+		var end: Float = inputs[3].get();
+		camera.data.raw.far_plane = end;
+		
+		}
+				
+		camera.buildProjection();
+
+		runOutput(0);
 	}
 }
