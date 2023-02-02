0abbd20c8f25d67e8088d67808728f69e514c611
Moritz Brückner <17685000+MoritzBrueckner@users.noreply.github.com>
2023-02-02 22:58:10 +0100

    Optimize RandomStringNode and improve its docstring

--- blender/arm/logicnode/random/LN_random_string.py
+++ blender/arm/logicnode/random/LN_random_string.py
@@ -2,7 +2,19 @@ from arm.logicnode.arm_nodes import *
 
 
 class RandomStringNode(ArmLogicTreeNode):
-    """Generates a random string based on a provided characters list."""
+    """Generate a random string based on a provided characters list.
+
+    @input Length: The length of the string to generate. If the length is
+        0 or negative, an empty string is returned.
+    @input Characters: A string containing the characters from which the
+        random generator can choose. For each letter in the output, the
+        generator randomly samples a character in the input string, so
+        the more often a character occurs in the input, the higher is
+        its chance of appearance in each letter of the result.
+        For example, if you provide `aaab` as a character string,
+        approximately 75% percent of the characters in all generated
+        strings are `a`, the remaining 25% are `b`.
+    """
     bl_idname = 'LNRandomStringNode'
     bl_label = 'Random String'
     arm_version = 1
