4a07cfe9a8d0c9397c428a2f69d1f7d72018631e
yvain <yvain29@gmail.com>
2023-02-10 16:09:04 +0100

    ssr_hald defaults to false

--- blender/arm/props_renderpath.py
+++ blender/arm/props_renderpath.py
@@ -493,7 +493,7 @@ class ArmRPListItem(bpy.types.PropertyGroup):
         name="Filter", description="Scaling filter", default='Linear')
     rp_dynres: BoolProperty(name="Dynamic Resolution", description="Dynamic resolution scaling for performance", default=False, update=update_renderpath)
     rp_chromatic_aberration: BoolProperty(name="Chromatic Aberration", description="Add chromatic aberration (scene fringe)", default=False, update=assets.invalidate_shader_cache)
-    arm_ssr_half_res: BoolProperty(name="Half Res", description="Trace in half resolution", default=True, update=update_renderpath)
+    arm_ssr_half_res: BoolProperty(name="Half Res", description="Trace in half resolution", default=False, update=update_renderpath)
     arm_voxelgi_dimensions: FloatProperty(name="Dimensions", description="Voxelization bounds",default=16, update=assets.invalidate_compiled_data)
     arm_voxelgi_revoxelize: BoolProperty(name="Revoxelize", description="Revoxelize scene each frame", default=False, update=assets.invalidate_shader_cache)
     arm_voxelgi_temporal: BoolProperty(name="Temporal Filter", description="Use temporal filtering to stabilize voxels", default=False, update=assets.invalidate_shader_cache)
