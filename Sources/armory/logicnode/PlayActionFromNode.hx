bce97b10601bdf895aa94871122c7a295d46ac64
RPaladin <69180012+rpaladin@users.noreply.github.com>
2023-02-14 18:38:24 -0800

    Update PlayActionFromNode.hx
    Co-Authored-By: QuantumCoderQC <55564981+QuantumCoderQC@users.noreply.github.com>

--- Sources/armory/logicnode/PlayActionFromNode.hx
+++ Sources/armory/logicnode/PlayActionFromNode.hx
@@ -1,31 +1,52 @@
 package armory.logicnode;
 
+import iron.object.Animation;
 import iron.object.Object;
 import iron.Scene;
 
 class PlayActionFromNode extends LogicNode {
 
+	var animation: Animation;
+	var startFrame: Int;
+	var endFrame: Int = -1;
+	var loop: Bool;
+
 	public function new(tree: LogicTree) {
 		super(tree);
+		tree.notifyOnUpdate(update);
+	}
+	
+	function update() {
+		if (animation != null) {
+			if (animation.currentFrame() == endFrame) {
+				if (loop) animation.setFrame(startFrame);
+				else {
+					if (!animation.paused) {
+						animation.pause();
+						runOutput(1);
+					}
+				}
+			}
+		}
 	}
 
 	override function run(from: Int) {
 		var object: Object = inputs[1].get();
 		var action: String = inputs[2].get();
-		var startFrame:Int = inputs[3].get();
-		var blendTime: Float = inputs[4].get();
-		var speed: Float = inputs[5].get();
-		var loop: Bool = inputs[6].get();
+		startFrame = inputs[3].get();
+		endFrame = inputs[4].get();
+		var blendTime: Float = inputs[5].get();
+		var speed: Float = inputs[6].get();
+		loop = inputs[7].get();
 		
-
 		if (object == null) return;
-		var animation = object.animation;
+		animation = object.animation;
 		if (animation == null) animation = object.getParentArmature(object.name);
 
 		animation.play(action, function() {
 			runOutput(1);
 		}, blendTime, speed, loop);
-		animation.update(startFrame*Scene.active.raw.frame_time);
+		animation.update(startFrame * Scene.active.raw.frame_time);
 
 		runOutput(0);
 	}
