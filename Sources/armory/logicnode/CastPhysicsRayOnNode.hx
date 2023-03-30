595b2f791f14a19c55d5732a80979397532f56b5
QuantumCoderQC <QuantumCoderQC@gmail.com>
2023-03-30 12:29:57 +0200

    implement ray cast on node

---
+++ Sources/armory/logicnode/CastPhysicsRayOnNode.hx
@@ -0,0 +1,56 @@
+package armory.logicnode;
+
+import iron.object.Object;
+import iron.math.Vec4;
+
+class CastPhysicsRayOnNode extends LogicNode {
+
+	var v = new Vec4();
+	var hitRb: Object = null;
+	var hitPos: Vec4 = null;
+	var hitNormal: Vec4 = null;
+
+	public function new(tree: LogicTree) {
+		super(tree);
+	}
+
+	function reset() {
+		hitRb = null;
+		hitPos = null;
+		hitNormal = null;
+	}
+
+	override function run(from:Int) {
+		reset();
+
+		var vfrom: Vec4 = inputs[1].get();
+		var vto: Vec4 = inputs[2].get();
+		var mask: Int = inputs[3].get();
+
+#if arm_physics
+		if (vfrom != null && vto != null) {
+			var physics = armory.trait.physics.PhysicsWorld.active;
+			var hit = physics.rayCast(vfrom, vto, mask);
+			if(hit != null) {
+				hitRb = hit.rb;
+				hitPos.setFrom(physics.hitPointWorld);
+				hitNormal.setFrom(physics.hitNormalWorld);
+			}
+		}
+#end
+		runOutput(0);
+	}
+
+	override function get(from: Int): Dynamic {
+		if (from == 1) { // Object
+			return hitRb;
+		}
+		else if (from == 2) { // Hit
+			return hitPos;
+		}
+		else { // Normal
+			return hitNormal;
+		}
+		return null;
+	}
+}
