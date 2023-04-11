584b7bf4e999491a266bd863c70e6943291510e2
Moritz Brückner <17685000+MoritzBrueckner@users.noreply.github.com>
2023-04-11 15:12:33 +0200

    Add component-wise mode to Vector Clamp node

--- Sources/armory/logicnode/VectorClampToSizeNode.hx
+++ Sources/armory/logicnode/VectorClampToSizeNode.hx
@@ -1,21 +1,32 @@
 package armory.logicnode;
 
 import iron.math.Vec4;
+import armory.math.Helper;
 
 class VectorClampToSizeNode extends LogicNode {
 
-	var v = new Vec4();
+	/** Clamping mode ("length" or "components"). **/
+	public var property0: String;
+
+	var v: Vec4;
 
 	public function new(tree: LogicTree) {
 		super(tree);
 	}
 
 	override function get(from: Int): Dynamic {
-		v = inputs[0].get();
+		v = inputs[0].get().clone();
 		var fmin: kha.FastFloat = inputs[1].get();
 		var fmax: kha.FastFloat = inputs[2].get();
 
-		v.clamp(fmin, fmax);
+		if (property0 == "length") {
+			v.clamp(fmin, fmax);
+		}
+		else if (property0 == "components") {
+			v.x = Helper.clamp(v.x, fmin, fmax);
+			v.y = Helper.clamp(v.y, fmin, fmax);
+			v.z = Helper.clamp(v.z, fmin, fmax);
+		}
 
 		return v;
 	}
