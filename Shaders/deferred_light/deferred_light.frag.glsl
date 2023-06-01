c6a2f6f7195147123653c15f76b683a34652fe26
yvain <yvain29@gmail.com>
2023-06-01 18:03:07 +0200

    dividing position by voxelSize

--- Shaders/deferred_light/deferred_light.frag.glsl
+++ Shaders/deferred_light/deferred_light.frag.glsl
@@ -230,8 +230,8 @@ void main() {
 	int clipmapLevel = int(max(log2(dist / maxClipmapSize), 0));
 	float clipmapLevelSize = pow(2.0, clipmapLevel) * voxelgiHalfExtents.x;
 	vec3 lookDirection = viewMatrix[2].xyz;
-	float voxelSize = clipmapLevelSize / voxelgiResolution.x * 16;// * 2 + clamp to 3 mipmaps
-	vec3 eyeSnap = floor(normalize(viewerPos + lookDirection * clipmapLevelSize) / voxelSize) * voxelSize;
+	float voxelSize = clipmapLevelSize / voxelgiResolution.x * 2;
+	vec3 eyeSnap = floor(normalize(viewerPos + lookDirection) / voxelSize) * voxelSize;
 	vec3 voxpos = (p - eyeSnap) / voxelSize;
 #endif
 
@@ -240,8 +240,8 @@ void main() {
 	int clipmapLevel = int(max(log2(dist / maxClipmapSize), 0));
 	float clipmapLevelSize = pow(2.0, clipmapLevel) * voxelgiHalfExtents.x;
 	vec3 lookDirection = viewMatrix[2].xyz;
-	float voxelSize = clipmapLevelSize / voxelgiResolution.x * 16;
-	vec3 eyeSnap = floor(normalize(viewerPos + lookDirection * clipmapLevelSize) / voxelSize) * voxelSize;
+	float voxelSize = clipmapLevelSize / voxelgiResolution.x * 2;
+	vec3 eyeSnap = floor(normalize(viewerPos + lookDirection) / voxelSize) * voxelSize;
 	vec3 voxpos = (p - eyeSnap) / voxelSize;
 #endif
 
