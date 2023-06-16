4f6669c4f450ca7f54571c904dec84b5fdb5d215
QuantumCoderQC <QuantumCoderQC@gmail.com>
2023-06-16 17:58:11 +0200

    Add option to recursivley set children visibility

--- blender/arm/logicnode/object/LN_set_object_visible.py
+++ blender/arm/logicnode/object/LN_set_object_visible.py
@@ -3,11 +3,19 @@ from arm.logicnode.arm_nodes import *
 class SetVisibleNode(ArmLogicTreeNode):
     """Sets whether the given object is visible.
 
+    @input Object: Object whose property to be set.
+
+    @input Visible: Visibility.
+
+    @input Children: Set the visibility of the children too. Visibility is set only to the immidiate children.
+
+    @input Recursive: If enabled, visibility of all the children in the tree is set. Ignored if `Children` is disabled.
+
     @seeNode Get Object Visible"""
     bl_idname = 'LNSetVisibleNode'
     bl_label = 'Set Object Visible'
     arm_section = 'props'
-    arm_version = 1
+    arm_version = 2
 
     property0: HaxeEnumProperty(
         'property0',
@@ -22,8 +30,15 @@ class SetVisibleNode(ArmLogicTreeNode):
         self.add_input('ArmNodeSocketObject', 'Object')
         self.add_input('ArmBoolSocket', 'Visible')
         self.add_input('ArmBoolSocket', 'Children', default_value=True)
+        self.add_input('ArmBoolSocket', 'Recursive', default_value=False)
 
         self.add_output('ArmNodeSocketAction', 'Out')
 
     def draw_buttons(self, context, layout):
         layout.prop(self, 'property0')
+
+    def get_replacement_node(self, node_tree: bpy.types.NodeTree):
+        if self.arm_version not in (0, 1):
+            raise LookupError()
+
+        return NodeReplacement.Identity(self)
