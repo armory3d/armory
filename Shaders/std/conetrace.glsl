cbb38b46bff8f3d2285181471de6399ca9534a84
Yvain Douard <i20@lenovo.home>
2023-07-14 15:50:47 +0200

    decreasing voxelSize in conetracing.

--- Shaders/std/conetrace.glsl
+++ Shaders/std/conetrace.glsl
@@ -144,7 +144,7 @@ vec3 traceRefraction(sampler3D voxels, const vec3 pos, const vec3 normal, const 
 float traceConeAO(sampler3D voxels, vec3 origin, vec3 dir, const float aperture, const float maxDist, const int clipmapLevel, const vec3 clipmapOffset) {
 	dir = normalize(dir);
     float sampleCol = 0.0;
-	float voxelSize = 0.125 * pow(2.0, clipmapLevel) / voxelgiResolution.x;
+	float voxelSize = 0.125 / (pow(2.0, clipmapLevel) * voxelgiResolution.x);
     float voxelSize0 = voxelSize * 2.0 * voxelgiOffset;
     float dist = voxelSize0;
     vec3 samplePos;
@@ -176,7 +176,7 @@ float traceConeAO(sampler3D voxels, vec3 origin, vec3 dir, const float aperture,
 float traceConeShadow(sampler3D voxels, const vec3 origin, vec3 dir, const float aperture, const float maxDist, const int clipmapLevel, const vec3 clipmapOffset) {
     dir = normalize(dir);
     float sampleCol = 0.0;
-	float voxelSize = 0.125 * pow(2.0, clipmapLevel) / voxelgiResolution.x;
+	float voxelSize = 0.125 / (pow(2.0, clipmapLevel) * voxelgiResolution.x);
     float voxelSize0 = voxelSize * 2.0 * voxelgiOffset;
     float dist = voxelSize0;
     vec3 samplePos;
