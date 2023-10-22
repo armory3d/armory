24e70c7a574ebc8d40c817a2c7534636db5586ef
t3du <32546729+t3du@users.noreply.github.com>
2023-10-22 11:23:58 -0300

    haxe code

---
+++ Sources/armory/logicnode/WaitForNode.hx
@@ -0,0 +1,18 @@
+package armory.logicnode;
+
+class WaitForNode extends LogicNode {
+
+	var froms: Array<Int> = [];
+
+	public function new(tree: LogicTree) {
+		super(tree);
+	}
+
+	override function run(from: Int) {
+
+		if(!froms.contains(from)) froms.push(from);
+		if(inputs.length == froms.length) runOutput(0);
+
+	}
+
+}
