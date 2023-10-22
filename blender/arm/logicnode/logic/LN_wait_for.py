e4aab712954d18a132889c0cc5aa6c8ff6f46a19
t3du <32546729+t3du@users.noreply.github.com>
2023-10-22 11:25:00 -0300

    py node

---
+++ blender/arm/logicnode/logic/LN_wait_for.py
@@ -0,0 +1,25 @@
+from arm.logicnode.arm_nodes import *
+
+class WaitForNode(ArmLogicTreeNode):
+    """Activates the output when all inputs are received."""
+    bl_idname = 'LNWaitForNode'
+    bl_label = 'Wait for Input'
+    arm_section = 'flow'
+    arm_version = 1
+    
+    def __init__(self):
+        super(WaitForNode, self).__init__()
+        array_nodes[str(id(self))] = self
+
+    def arm_init(self, context):
+        self.add_output('ArmNodeSocketAction', 'Out')
+
+    def draw_buttons(self, context, layout):
+        row = layout.row(align=True)
+
+        op = row.operator('arm.node_add_input', text='New', icon='PLUS', emboss=True)
+        op.node_index = str(id(self))
+        op.socket_type = 'ArmNodeSocketAction'
+        op2 = row.operator('arm.node_remove_input', text='', icon='X', emboss=True)
+        op2.node_index = str(id(self))
+
