12a1c3c1d63577ab6d34357c1107c804fec20f1b
e2002e <yvain29@gmail.com>
2023-10-23 20:05:01 +0200

    correct voxelization with dimentions options but can't render small scenes.

--- Shaders/deferred_light/deferred_light.frag.glsl
+++ Shaders/deferred_light/deferred_light.frag.glsl
@@ -223,18 +223,18 @@ void main() {
 
 #ifdef _VoxelGI
 	float dist = max(abs(p.x - viewerPos.x), max(abs(p.y - viewerPos.y), abs(p.z - viewerPos.z)));
-	int clipmapLevel = int(max(log2(dist / voxelgiResolution.x), 0));
-	float voxelSize = pow(2.0, clipmapLevel) * 2.0;
-	vec3 eyeSnap = floor((viewerPos + eyeLook * voxelgiHalfExtents.x * pow(2.0, clipmapLevel)) / voxelSize) * voxelSize;
-    vec3 voxpos = (p - eyeSnap) / voxelSize * 0.5 / voxelgiHalfExtents.x;
+	int clipmapLevel = int(max(log2(dist / voxelgiHalfExtents.x * 0.125), 0));
+	float voxelSize = pow(2.0, clipmapLevel) * 4.0;
+	vec3 eyeSnap = floor((viewerPos + eyeLook) / voxelSize) * voxelSize;
+    vec3 voxpos = (p - eyeSnap) / voxelSize * 0.25 / voxelgiHalfExtents.x;
 #endif
 
 #ifdef _VoxelAOvar
 	float dist = max(abs(p.x - viewerPos.x), max(abs(p.y - viewerPos.y), abs(p.z - viewerPos.z)));
-	int clipmapLevel = int(max(log2(dist / voxelgiResolution.x), 0));
-	float voxelSize = pow(2.0, clipmapLevel) * 2.0;
-	vec3 eyeSnap = floor((viewerPos + eyeLook * voxelgiHalfExtents.x * pow(2.0, clipmapLevel)) / voxelSize) * voxelSize;
-    vec3 voxpos = (p - eyeSnap) / voxelSize * 0.5 / voxelgiHalfExtents.x;
+	int clipmapLevel = int(max(log2(dist / voxelgiHalfExtents.x * 0.125), 0));
+	float voxelSize = pow(2.0, clipmapLevel) * 4.0;
+	vec3 eyeSnap = floor((viewerPos + eyeLook) / voxelSize) * voxelSize;
+    vec3 voxpos = (p - eyeSnap) / voxelSize * 0.25 / voxelgiHalfExtents.x;
 #endif
 
 #ifdef _VoxelRefract
