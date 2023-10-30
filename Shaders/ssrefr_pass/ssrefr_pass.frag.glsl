65ad3802fd5dd6a4a1e75e38def34f2549b81238
e2002e <yvain29@gmail.com>
2023-10-30 12:00:38 +0100

    keep depth variable global

--- Shaders/ssrefr_pass/ssrefr_pass.frag.glsl
+++ Shaders/ssrefr_pass/ssrefr_pass.frag.glsl
@@ -40,7 +40,7 @@ vec2 getProjectedCoord(const vec3 hit) {
 }
 
 float getDeltaDepth(const vec3 hit) {
-    float depth = textureLod(gbufferD, getProjectedCoord(hit), 0.0).r * 2.0 - 1.0;
+    depth = textureLod(gbufferD, getProjectedCoord(hit), 0.0).r * 2.0 - 1.0;
     vec3 viewPos = normalize(getPosView(viewRay, depth, cameraProj));
     return viewPos.z - hit.z;
 }
