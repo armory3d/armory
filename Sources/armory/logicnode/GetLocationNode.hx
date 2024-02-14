357aef589184caf18cdc542681c15d439593dea5
ARIEL GIMENEZ <arielgimenez_95@hotmail.com>
2024-02-14 08:27:14 -0300

    GetLocationNode Static methods in the nodes so that the nodes API can be used within HAXE

--- Sources/armory/logicnode/GetLocationNode.hx
+++ Sources/armory/logicnode/GetLocationNode.hx
@@ -12,20 +12,26 @@ class GetLocationNode extends LogicNode {
 		var object: Object = inputs[0].get();
 		var relative: Bool = inputs[1].get();
 
+		return GetObjectLocation(  object, relative );
+	}
+
+	//STATIC CLASS
+	public static function GetObjectLocation(  object : Object, relative : Bool  ) {
+	
 		if (object == null) return null;
-
+	
 		var loc = object.transform.world.getLoc();
-
+	
 		if (relative && object.parent != null) {
 			loc.sub(object.parent.transform.world.getLoc()); // Add parent location influence
-
+	
 			// Convert loc to parent local space
-			var dotX = loc.dot(object.parent.transform.right());
-			var dotY = loc.dot(object.parent.transform.look());
-			var dotZ = loc.dot(object.parent.transform.up());
+			var dotX:Float = loc.dot(object.parent.transform.right());
+			var dotY:Float = loc.dot(object.parent.transform.look());
+			var dotZ:Float = loc.dot(object.parent.transform.up());
 			loc.set(dotX, dotY, dotZ);
 		}
-
+	
 		return loc;
 	}
 }
