e766479ce8d55d83c4487ce9b5aae25c03b85b1c
e2002e <yvain29@gmail.com>
2024-04-11 03:44:23 +0200

    clear voxels light each frame in forward render.

--- Sources/armory/renderpath/RenderPathForward.hx
+++ Sources/armory/renderpath/RenderPathForward.hx
@@ -394,6 +394,7 @@ class RenderPathForward {
 				#if (rp_voxels == "Voxel GI")
 				path.clearImage("voxelsNor", 0x00000000);
 				path.clearImage("voxelsEmission", 0x00000000);
+				path.clearImage("voxelsLight", 0x00000000);
 				#end
 				path.clearImage("voxels", 0x00000000);
 				Inc.computeVoxelsOffsetPrev();
