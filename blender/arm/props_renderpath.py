6f3e607aac7701e927d4e078114655983ac57395
yvain <yvain29@gmail.com>
2024-08-01 11:15:33 +0200

    add 16^3 voxels res

--- blender/arm/props_renderpath.py
+++ blender/arm/props_renderpath.py
@@ -488,7 +488,8 @@ class ArmRPListItem(bpy.types.PropertyGroup):
                ],
         name="Voxels", description="Dynamic global illumination", default='Off', update=update_renderpath)
     rp_voxelgi_resolution: EnumProperty(
-        items=[('32', '32', '32'),
+        items=[('16', '16', '16'),
+               ('32', '32', '32'),
                ('64', '64', '64'),
                ('128', '128', '128'),
                ('256', '256', '256'),
