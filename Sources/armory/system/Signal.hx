edc8610eda47f2a73c8684f4c00f7725fe73b0e9
Moisés Peláez <moises.jpelaez@gmail.com>
2024-10-28 18:17:04 -0400

    Add Signals

---
+++ Sources/armory/system/Signal.hx
@@ -0,0 +1,39 @@
+package armory.system;
+
+import haxe.Constraints.Function;
+
+class Signal {
+    var callbacks:Array<Function> = [];
+
+    public function new() {
+        
+    }
+    
+    public function connect(callback:Function) {
+        if (!callbacks.contains(callback)) callbacks.push(callback);
+    }
+    
+    public function disconnect(callback:Function) {
+        if (callbacks.contains(callback)) callbacks.remove(callback);
+    }
+
+    public function emit(...args:Any) {
+        for (callback in callbacks) Reflect.callMethod(this, callback, args);
+    }
+
+    public function getConnections():Array<Function> {
+        return callbacks;
+    }
+
+    public function getObject():Int {
+        return object;
+    }
+
+    public function isConnected(callBack:Function):Bool {
+        return callbacks.contains(callBack);
+    }
+
+    public function isNull():Bool {
+        return callbacks.length == 0;
+    }
+}
