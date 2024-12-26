2c3f1ee94719ba45c89eb4aecc52922b57faec42
t3du <32546729+t3du@users.noreply.github.com>
2024-12-25 21:44:52 -0300

    Add files via upload

--- armory/blender/arm/logicnode/material/LN_get_object_materials.py
+++ armory/blender/arm/logicnode/material/LN_get_object_materials.py
@@ -1,13 +1,13 @@
 from arm.logicnode.arm_nodes import *
 
-class GetMaterialNode(ArmLogicTreeNode):
-    """Returns the material of the given object."""
-    bl_idname = 'LNGetMaterialNode'
-    bl_label = 'Get Object Material'
+class GetMaterialsNode(ArmLogicTreeNode):
+    """Returns the materials of the given object."""
+    bl_idname = 'LNGetMaterialsNode'
+    bl_label = 'Get Object Materials'
     arm_version = 1
 
     def arm_init(self, context):
         self.add_input('ArmNodeSocketObject', 'Object')
-        self.add_input('ArmIntSocket', 'Slot')
 
-        self.add_output('ArmDynamicSocket', 'Material')
+        self.add_output('ArmNodeSocketArray', 'Materials')
+        self.add_output('ArmIntSocket', 'Length')
