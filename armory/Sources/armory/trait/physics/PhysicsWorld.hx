83b6694d0ca76e30c1e077e393d51f383bd63d40
Moisés Peláez <moises.jpelaez@gmail.com>
2025-05-07 17:44:33 -0400

    Add Hit typedef for PhysicsWorld

--- armory/Sources/armory/trait/physics/PhysicsWorld.hx
+++ armory/Sources/armory/trait/physics/PhysicsWorld.hx
@@ -2,17 +2,24 @@ package armory.trait.physics;
 
 #if (!arm_physics)
 
-class PhysicsWorld extends iron.Trait { public function new() { super(); } }
+class Hit {
+}
+
+class PhysicsWorld extends iron.Trait {
+	public function new() { super(); }
+}
 
 #else
 
 	#if arm_bullet
 
 	typedef PhysicsWorld = armory.trait.physics.bullet.PhysicsWorld;
+	typedef Hit = armory.trait.physics.bullet.PhysicsWorld.Hit;
 
 	#else
 
 	typedef PhysicsWorld = armory.trait.physics.oimo.PhysicsWorld;
+	typedef Hit = armory.trait.physics.oimo.PhysicsWorld.Hit;
 
 	#end
 
