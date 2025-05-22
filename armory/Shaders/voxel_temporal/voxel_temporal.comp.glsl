0dda38a16b0107e0ae3130bae7273b96c6ee092e
e2002e <yvain29@gmail.com>
2025-05-22 20:17:52 +0200

    use fibonnaci hemisphere cone directions. Disable voxels shadows in voxelizer

--- armory/Shaders/voxel_temporal/voxel_temporal.comp.glsl
+++ armory/Shaders/voxel_temporal/voxel_temporal.comp.glsl
@@ -98,31 +98,31 @@ void main() {
 			int count = int(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 15)));
 			if (count > 0) {
 				vec4 basecol = vec4(0.0);
-				basecol.r = float(imageLoad(voxels, src)) / 255;
-				basecol.g = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x))) / 255;
-				basecol.b = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 2))) / 255;
-				basecol.a = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 3))) / 255;
+				basecol.r = float(imageLoad(voxels, src)) / 1024;
+				basecol.g = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x))) / 1024;
+				basecol.b = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 2))) / 1024;
+				basecol.a = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 3))) / 1024;
 				basecol /= count;
 				vec3 emission = vec3(0.0);
-				emission.r = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 4))) / 255;
-				emission.g = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 5))) / 255;
-				emission.b = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 6))) / 255;
+				emission.r = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 4))) / 1024;
+				emission.g = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 5))) / 1024;
+				emission.b = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 6))) / 1024;
 				emission /= count;
 				vec3 N = vec3(0.0);
-				N.r = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 7))) / 255.0;
-				N.g = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 8))) / 255.0;
+				N.r = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 7))) / 1024.0;
+				N.g = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 8))) / 1024.0;
 				N /= count;
 				N = decode_oct(N.rg * 2.0 - 1.0);
 
 				vec3 envl = vec3(0.0);
-				envl.r = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 9))) / 255;
-				envl.g = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 10))) / 255;
-				envl.b = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 11))) / 255;
+				envl.r = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 9))) / 1024;
+				envl.g = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 10))) / 1024;
+				envl.b = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 11))) / 1024;
 				envl /= count;
 				vec3 light = vec3(0.0);
-				light.r = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 12))) / 255;
-				light.g = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 13))) / 255;
-				light.b = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 14))) / 255;
+				light.r = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 12))) / 1024;
+				light.g = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 13))) / 1024;
+				light.b = float(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x * 14))) / 1024;
 				light /= count;
 
 				//clipmap to world
@@ -141,7 +141,7 @@ void main() {
 			#else
 			int count = int(imageLoad(voxels, src + ivec3(0, 0, voxelgiResolution.x)));
 			if (count > 0) {
-				opac = float(imageLoad(voxels, src)) / 255;
+				opac = float(imageLoad(voxels, src)) / 1024;
 				opac /= count;
 			}
 			#endif
