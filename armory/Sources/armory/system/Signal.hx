8b38aadd22b5ab2e9f2645d4b10b1f616bbafb10
Moisés Peláez <moises.jpelaez@gmail.com>
2025-08-13 02:33:35 -0400

    Fix emit calls when callbacks array mutates

--- armory/Sources/armory/system/Signal.hx
+++ armory/Sources/armory/system/Signal.hx
@@ -3,33 +3,37 @@ package armory.system;
 import haxe.Constraints.Function;
 
 class Signal {
-    var callbacks:Array<Function> = [];
+    var callbacks: Array<Function> = [];
 
     public function new() {
-        
+
     }
-    
-    public function connect(callback:Function) {
+
+    public function connect(callback: Function) {
         if (!callbacks.contains(callback)) callbacks.push(callback);
     }
-    
-    public function disconnect(callback:Function) {
+
+    public function disconnect(callback: Function) {
         if (callbacks.contains(callback)) callbacks.remove(callback);
     }
 
-    public function emit(...args:Any) {
-        for (callback in callbacks) Reflect.callMethod(this, callback, args);
+    public function emit(...args: Any) {
+        var callbacksCopy: Array<Function> = callbacks.copy();
+        for (callback in callbacksCopy) {
+            if (!callbacks.contains(callback)) continue;
+            Reflect.callMethod(this, callback, args);
+        }
     }
 
-    public function getConnections():Array<Function> {
+    public function getConnections(): Array<Function> {
         return callbacks;
     }
 
-    public function isConnected(callBack:Function):Bool {
+    public function isConnected(callBack: Function): Bool {
         return callbacks.contains(callBack);
     }
 
-    public function isNull():Bool {
+    public function isNull(): Bool {
         return callbacks.length == 0;
     }
 }
