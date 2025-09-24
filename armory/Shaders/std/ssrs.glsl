a9acc2e2d4ed67093228f0a69ce9a5ceee3174ce
e2002e <yvain29@gmail.com>
2025-09-24 18:49:43 +0200

    Update ssrs.glsl

--- armory/Shaders/std/ssrs.glsl
+++ armory/Shaders/std/ssrs.glsl
@@ -7,12 +7,6 @@ uniform mat4 VP;
 const int   ssrsBinarySteps = 7;  // refinement steps
 const float ssrsJitter = 0.125;      // per-pixel randomization scale
 
-uniform vec2 dirInv;
-
-// const float blurWeights[5] = float[] (0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216);
-const float blurWeights[10] = float[] (0.132572, 0.125472, 0.106373, 0.08078, 0.05495, 0.033482, 0.018275, 0.008934, 0.003912, 0.001535);
-const float discardThreshold = 0.95;
-
 // Projects world position to screen space
 vec2 getProjectedCoord(vec3 hitCoord) {
     vec4 projectedCoord = VP * vec4(hitCoord, 1.0);
@@ -100,47 +94,6 @@ float traceShadowSS(vec3 dir, vec3 hitCoord, sampler2D gbufferD, sampler2D gbuff
 	}
 
     shadowSum /= float(ssrsSamples);
-
-    // If completely unshadowed, skip expensive blur
-    if (shadowSum > 0.99) return shadowSum;
-
-    vec3 nor = getNor(textureLod(gbuffer0, texCoord, 0.0).rg);
-    float final = shadowSum;
-    float weight = 1.0;
-
-    // Use a proper Gaussian-like kernel
-    float blurWeights[4] = float[](0.4, 0.3, 0.2, 0.1);
-    vec2 blurOffsets[4] = vec2[](
-        vec2(1.0, 0.0),
-        vec2(0.0, 1.0),
-        vec2(1.0, 1.0),
-        vec2(1.0, -1.0)
-    );
-
-    for (int i = 0; i < 4; ++i) {
-        vec2 offs = blurOffsets[i] * dirInv;
-
-        // Get normals at offset positions
-        vec3 nor1 = getNor(textureLod(gbuffer0, texCoord + offs, 0.0).rg);
-        vec3 nor2 = getNor(textureLod(gbuffer0, texCoord - offs, 0.0).rg);
-
-        float inf1 = step(discardThreshold, dot(nor1, nor));
-        float inf2 = step(discardThreshold, dot(nor2, nor));
-
-        // Only sample if normals are similar
-        if (inf1 > 0.0) {
-            float col1 = trace(dir, hitCoord, gbufferD, invVP, eye, rand + offs);
-            final += col1 * blurWeights[i];
-            weight += blurWeights[i];
-        }
-        if (inf2 > 0.0) {
-            float col2 = trace(dir, hitCoord, gbufferD, invVP, eye, rand - offs);
-            final += col2 * blurWeights[i];
-            weight += blurWeights[i];
-        }
-    }
-
-    shadowSum = final / weight;
     return shadowSum;
 }
 
